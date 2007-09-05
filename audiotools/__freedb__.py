dc2628b9ae4025f9d8df0c1d1daf771c2d28ea7b
Brian Langenberger <brian@localhost.localdomain>
2007-09-04 19:40:37 -0500

    More fixes for the Great Split.

--- audiotools/__freedb__.py
+++ audiotools/__freedb__.py
@@ -18,7 +18,7 @@
 #Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
 
-from audiotools import VERSION,Con,cStringIO,sys,re
+from audiotools import VERSION,Con,cStringIO,sys,re,MetaData,AlbumMetaData
         
 #######################
 #XMCD
--- audiotools/__id3__.py
+++ audiotools/__id3__.py
@@ -17,8 +17,7 @@
 #along with this program; if not, write to the Free Software
 #Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
-
-from audiotools import MetaData,Con,re
+from audiotools import MetaData,Con,re,os
 
 class EndOfID3v2Stream(Exception): pass
 class UnsupportedID3v2Version(Exception): pass
