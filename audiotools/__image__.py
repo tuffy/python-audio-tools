d245923a2253267724b954c3de649d18da415764
Brian Langenberger <bjl@usa.net>
2007-09-13 08:53:09 -0500

    Added PNG support.

--- audiotools/__image__.py
+++ audiotools/__image__.py
@@ -31,8 +31,9 @@ class ImageMetrics:
         self.color_count = color_count
         self.mime_type = mime_type
 
+class InvalidImage(Exception): pass
 
-class InvalidJPEG(Exception): pass
+class InvalidJPEG(InvalidImage): pass
 
 class JPEG(ImageMetrics):
     SEGMENT_HEADER = Con.Struct('segment_header',
@@ -70,28 +71,97 @@ class JPEG(ImageMetrics):
     @classmethod
     def parse(cls, file):
         frame0 = None
-        
-        header = cls.SEGMENT_HEADER.parse_stream(file)
-        if (header.type != 0xD8):
-            raise InvalidJPEG('invalid JPEG header')
-        
-        segment = cls.SEGMENT_HEADER.parse_stream(file)
-        while (segment.type != 0xD9):
-            if (segment.type == 0xDA):
-                break
 
-            if (segment.type == 0xE0):
-                jfif_segment_marker = cls.APP0.parse_stream(file)
-                file.seek(segment.length - cls.APP0.sizeof() - 2,1)
-            elif (segment.type == 0xC0):
-                frame0 = cls.SOF0.parse_stream(file)
-            else:
-                file.seek(segment.length - 2,1)
+        try:
+            header = cls.SEGMENT_HEADER.parse_stream(file)
+            if (header.type != 0xD8):
+                raise InvalidJPEG('invalid JPEG header')
+
             segment = cls.SEGMENT_HEADER.parse_stream(file)
+            while (segment.type != 0xD9):
+                if (segment.type == 0xDA):
+                    break
 
-        if (frame0 is not None):
-            return JPEG(width = frame0.image_width,
-                        height = frame0.image_height,
-                        bits_per_pixel = (frame0.data_precision * \
-                                          len(frame0.components)))
+                if (segment.type == 0xE0):
+                    jfif_segment_marker = cls.APP0.parse_stream(file)
+                    file.seek(segment.length - cls.APP0.sizeof() - 2,1)
+                elif (segment.type == 0xC0):
+                    frame0 = cls.SOF0.parse_stream(file)
+                else:
+                    file.seek(segment.length - 2,1)
+                segment = cls.SEGMENT_HEADER.parse_stream(file)
 
+            if (frame0 is not None):
+                return JPEG(width = frame0.image_width,
+                            height = frame0.image_height,
+                            bits_per_pixel = (frame0.data_precision * \
+                                              len(frame0.components)))
+        except Con.ConstError:
+            raise InvalidJPEG('invalid JPEG')
+
+class InvalidPNG(InvalidImage): pass
+
+class PNG(ImageMetrics):
+    HEADER = Con.Const(Con.String('header',8),'\x89PNG\r\n\x1a\n')
+    CHUNK_HEADER = Con.Struct('chunk',
+                              Con.UBInt32('length'),
+                              Con.String('type',4))
+    CHUNK_FOOTER = Con.Struct('crc32',
+                              Con.UBInt32('crc'))
+
+    IHDR = Con.Struct('IHDR',
+                      Con.UBInt32('width'),
+                      Con.UBInt32('height'),
+                      Con.Byte('bit_depth'),
+                      Con.Byte('color_type'),
+                      Con.Byte('compression_method'),
+                      Con.Byte('filter_method'),
+                      Con.Byte('interlace_method'))
+
+    def __init__(self, width, height, bits_per_pixel, color_count):
+        ImageMetrics.__init__(self, width, height, bits_per_pixel, color_count,
+                              'image/png')
+
+    @classmethod
+    def parse(cls, file):
+        ihdr = None
+        plte = None
+
+        try:
+            header = cls.HEADER.parse_stream(file)
+
+            chunk_header = cls.CHUNK_HEADER.parse_stream(file)
+            data = file.read(chunk_header.length)
+            chunk_footer = cls.CHUNK_FOOTER.parse_stream(file)
+            while (chunk_header.type != 'IEND'):
+                if (chunk_header.type == 'IHDR'):
+                    ihdr = cls.IHDR.parse(data)
+                elif (chunk_header.type == 'PLTE'):
+                    plte = data
+
+                chunk_header = cls.CHUNK_HEADER.parse_stream(file)
+                data = file.read(chunk_header.length)
+                chunk_footer = cls.CHUNK_FOOTER.parse_stream(file)
+
+            if (ihdr.color_type == 0):   #grayscale
+                bits_per_pixel = ihdr.bit_depth
+                color_count = 0
+            elif (ihdr.color_type == 2): #RGB
+                bits_per_pixel = ihdr.bit_depth * 3
+                color_count = 0
+            elif (ihdr.color_type == 3): #palette
+                bits_per_pixel = 24
+                if ((len(plte) % 3) != 0):
+                    raise InvalidPNG('invalid PLTE chunk length')
+                else:
+                    color_count = len(plte) / 3
+            elif (ihdr.color_type == 4): #grayscale + alpha
+                bits_per_pixel = ihdr.bit_depth * 2
+                color_count = 0
+            elif (ihdr.color_type == 6): #RGB + alpha
+                bits_per_pixel = ihdr.bit_depth * 4
+                color_count = 0
+
+            return PNG(ihdr.width,ihdr.height,bits_per_pixel,color_count)
+        except Con.ConstError:
+            raise InvalidPNG('invalid PNG')
