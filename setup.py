ffff991ce0bf1cc313b07852ad145522db1c7eff
Brian Langenberger <bjl@usa.net>
2008-02-03 19:39:50 -0600

    Updated the version number.

--- setup.py
+++ setup.py
@@ -86,7 +86,7 @@ extensions = [cdiomodule,
 #     pass #pkg-config not available
 
 setup (name = 'Python Audio Tools',
-       version = '2.7beta1',
+       version = '2.7',
        description = 'A collection of audio handling utilities',
        author = 'Brian Langenberger',
        author_email = 'tuffy@users.sourceforge.net',
