ed641af868188d40aa45f321b9ac486763c0fbc7
brian <bjl@usa.net>
2009-01-28 20:18:20 -0600

    Version detection fix.

--- audiotools/__init__.py
+++ audiotools/__init__.py
@@ -20,7 +20,7 @@
 
 import sys
 
-if (sys.version_info < (2,5.0,'final',0)):
+if (sys.version_info < (2,5,0,'final',0)):
     print >>sys.stderr,"*** Python 2.5.0 or better required"
     sys.exit(1)
 
