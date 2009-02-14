c708e7ae493f8199a7600efb56930b56444f0e35
Brian Langenberger <bjl@usa.net>
2009-02-14 11:22:26 -0600

    Version number to 2.11

--- audiotools/__init__.py
+++ audiotools/__init__.py
@@ -98,7 +98,7 @@ FREEDB_PORT = config.getint_default("FreeDB","port",80)
 THUMBNAIL_FORMAT = config.get_default("Thumbnail","format","jpeg")
 THUMBNAIL_SIZE = config.getint_default("Thumbnail","size",150)
 
-VERSION = "2.11beta1"
+VERSION = "2.11"
 
 FILENAME_FORMAT = config.get_default(
     "Filenames","format",
