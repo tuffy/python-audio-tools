94412ca8dfea25bf68c1a8a1238cd028b9af205d
Brian Langenberger <bjl@usa.net>
2009-03-14 19:05:38 -0500

    Convert coverview to timer-based thumbnail resizing. Instead of performing a full resize at every TreeView resize event, a flag is polled at regular intervals and a resize is performed if set. Nobody cares if thumbnails aren't updated in real time, but people will care if the whole app slows to a crawl every time the vertical bar is dragged around.

--- audiotools/__init__.py
+++ audiotools/__init__.py
@@ -98,7 +98,7 @@ FREEDB_PORT = config.getint_default("FreeDB","port",80)
 THUMBNAIL_FORMAT = config.get_default("Thumbnail","format","jpeg")
 THUMBNAIL_SIZE = config.getint_default("Thumbnail","size",150)
 
-VERSION = "2.11"
+VERSION = "2.12alpha1"
 
 FILENAME_FORMAT = config.get_default(
     "Filenames","format",
--- setup.py
+++ setup.py
@@ -91,7 +91,7 @@ extensions = [cdiomodule,
 #     pass #pkg-config not available
 
 setup (name = 'Python Audio Tools',
-       version = '2.11',
+       version = '2.12alpha1',
        description = 'A collection of audio handling utilities',
        author = 'Brian Langenberger',
        author_email = 'tuffy@users.sourceforge.net',
