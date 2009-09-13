2c2475a78ccc8b5205879aa4509a7d576a0cfe12
Brian Langenberger <bjl@usa.net>
2009-09-13 10:01:00 -0500

    Add cdinfo utility.

--- audiotools/__init__.py
+++ audiotools/__init__.py
@@ -97,7 +97,7 @@ MUSICBRAINZ_PORT = config.getint_default("MusicBrainz","port",80)
 THUMBNAIL_FORMAT = config.get_default("Thumbnail","format","jpeg")
 THUMBNAIL_SIZE = config.getint_default("Thumbnail","size",150)
 
-VERSION = "2.13alpha2"
+VERSION = "2.13alpha3"
 
 FILENAME_FORMAT = config.get_default(
     "Filenames","format",
