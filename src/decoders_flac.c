3368e5af5a27945315de16343ed1efecb956e4a6
Brian Langenberger <brian@laptop.(none)>
2009-12-05 08:49:21 -0600

    Split FLAC-specific decoding into seperate files.

---
+++ src/decoders_flac.c
@@ -0,0 +1,104 @@
+int FlacDecoder_init(decoders_FlacDecoder *self,
+		     PyObject *args, PyObject *kwds) {
+  char* filename;
+
+  if (!PyArg_ParseTuple(args, "s", &filename))
+    return -1;
+
+  self->filename = NULL;
+  self->file = NULL;
+  self->bitstream = NULL;
+
+  /*open the flac file*/
+  self->file = fopen(filename,"rb");
+  if (self->file == NULL) {
+    PyErr_SetFromErrnoWithFilename(PyExc_IOError,filename);
+    return -1;
+  } else {
+    self->bitstream = bs_open(self->file);
+  }
+
+  self->filename = strdup(filename);
+
+  if (!FlacDecoder_read_metadata(self)) {
+    return -1;
+  }
+
+  return 0;
+}
+
+void FlacDecoder_dealloc(decoders_FlacDecoder *self) {
+  if (self->filename != NULL)
+    free(self->filename);
+
+  bs_close(self->bitstream);
+
+  Py_TYPE(self)->tp_free((PyObject*)self);
+}
+
+PyObject *FlacDecoder_new(PyTypeObject *type,
+			  PyObject *args, PyObject *kwds) {
+  decoders_FlacDecoder *self;
+
+  self = (decoders_FlacDecoder *)type->tp_alloc(type, 0);
+
+  return (PyObject *)self;
+}
+
+int FlacDecoder_read_metadata(decoders_FlacDecoder *self) {
+  unsigned int last_block;
+  unsigned int block_type;
+  unsigned int block_length;
+
+  if (read_bits(self->bitstream,32) != 0x664C6143u) {
+    PyErr_SetString(PyExc_ValueError,"not a FLAC file");
+    return 0;
+  }
+
+  last_block = read_bits(self->bitstream,1);
+  block_type = read_bits(self->bitstream,7);
+  block_length = read_bits(self->bitstream,24);
+
+  if (block_type == 0) {
+    self->streaminfo.minimum_block_size = read_bits(self->bitstream,16);
+    self->streaminfo.maximum_block_size = read_bits(self->bitstream,16);
+    self->streaminfo.minimum_frame_size = read_bits(self->bitstream,24);
+    self->streaminfo.maximum_frame_size = read_bits(self->bitstream,24);
+    self->streaminfo.sample_rate = read_bits(self->bitstream,20);
+    self->streaminfo.channels = read_bits(self->bitstream,3) + 1;
+    self->streaminfo.bits_per_sample = read_bits(self->bitstream,5) + 1;
+    self->streaminfo.total_samples = read_bits64(self->bitstream,36);
+    if (fread(self->streaminfo.md5sum,sizeof(unsigned char),16,self->file)
+	!= 16) {
+      PyErr_SetString(PyExc_ValueError,"unable to read md5sum");
+      return 0;
+    }
+  } else {
+    PyErr_SetString(PyExc_ValueError,"STREAMINFO not first metadata block");
+    return 0;
+  }
+
+  while (!last_block) {
+    last_block = read_bits(self->bitstream,1);
+    block_type = read_bits(self->bitstream,7);
+    block_length = read_bits(self->bitstream,24);
+    fseek(self->file,block_length,SEEK_CUR);
+  }
+
+  return 1;
+}
+
+static PyObject *FlacDecoder_sample_rate(decoders_FlacDecoder *self,
+					 void *closure) {
+  return Py_BuildValue("i",self->streaminfo.sample_rate);
+}
+
+static PyObject *FlacDecoder_bits_per_sample(decoders_FlacDecoder *self,
+					     void *closure) {
+  return Py_BuildValue("i",self->streaminfo.bits_per_sample);
+}
+
+static PyObject *FlacDecoder_channels(decoders_FlacDecoder *self,
+				      void *closure) {
+  return Py_BuildValue("i",self->streaminfo.channels);
+}
---
+++ src/decoders_flac.h
@@ -0,0 +1,143 @@
+struct flac_STREAMINFO {
+  uint16_t minimum_block_size;
+  uint16_t maximum_block_size;
+  uint32_t minimum_frame_size;
+  uint32_t maximum_frame_size;
+  uint32_t sample_rate;
+  uint8_t channels;
+  uint8_t bits_per_sample;
+  uint64_t total_samples;
+  unsigned char md5sum[16];
+};
+
+typedef struct {
+  PyObject_HEAD
+  char* filename;
+
+  FILE* file;
+  Bitstream* bitstream;
+
+  struct flac_STREAMINFO streaminfo;
+} decoders_FlacDecoder;
+
+static PyObject *FlacDecoder_sample_rate(decoders_FlacDecoder *self,
+					 void *closure);
+static PyObject *FlacDecoder_bits_per_sample(decoders_FlacDecoder *self,
+					     void *closure);
+
+static PyObject *FlacDecoder_channels(decoders_FlacDecoder *self,
+				      void *closure);
+
+int FlacDecoder_init(decoders_FlacDecoder *self,
+		     PyObject *args, PyObject *kwds);
+
+PyGetSetDef FlacDecoder_getseters[] = {
+  {"sample_rate",
+   (getter)FlacDecoder_sample_rate, NULL, "sample rate", NULL},
+  {"bits_per_sample",
+   (getter)FlacDecoder_bits_per_sample, NULL, "bits per sample", NULL},
+  {"channels",
+   (getter)FlacDecoder_channels, NULL, "channels", NULL},
+  {NULL}
+};
+
+PyMethodDef FlacDecoder_methods[] = {
+  {NULL}
+};
+
+void FlacDecoder_dealloc(decoders_FlacDecoder *self);
+
+PyObject *FlacDecoder_new(PyTypeObject *type,
+			  PyObject *args, PyObject *kwds);
+
+int FlacDecoder_read_metadata(decoders_FlacDecoder *self);
+
+#ifdef IS_PY3K
+
+static PyTypeObject decoders_FlacDecoderType = {
+    PyVarObject_HEAD_INIT(NULL, 0)
+    "decoders.FlacDecoder",     /* tp_name */
+    sizeof(decoders_FlacDecoder), /* tp_basicsize */
+    0,                         /* tp_itemsize */
+    (destructor)FlacDecoders_dealloc, /* tp_dealloc */
+    0,                         /* tp_print */
+    0,                         /* tp_getattr */
+    0,                         /* tp_setattr */
+    0,                         /* tp_reserved */
+    0,                         /* tp_repr */
+    0,                         /* tp_as_number */
+    0,                         /* tp_as_sequence */
+    0,                         /* tp_as_mapping */
+    0,                         /* tp_hash  */
+    0,                         /* tp_call */
+    0,                         /* tp_str */
+    0,                         /* tp_getattro */
+    0,                         /* tp_setattro */
+    0,                         /* tp_as_buffer */
+    Py_TPFLAGS_DEFAULT |
+        Py_TPFLAGS_BASETYPE,   /* tp_flags */
+    "FlacDecoder objects",     /* tp_doc */
+    0,		               /* tp_traverse */
+    0,		               /* tp_clear */
+    0,		               /* tp_richcompare */
+    0,		               /* tp_weaklistoffset */
+    0,		               /* tp_iter */
+    0,		               /* tp_iternext */
+    FlacDecoder_methods,       /* tp_methods */
+    0,                         /* tp_members */
+    FlacDecoder_getseters,     /* tp_getset */
+    0,                         /* tp_base */
+    0,                         /* tp_dict */
+    0,                         /* tp_descr_get */
+    0,                         /* tp_descr_set */
+    0,                         /* tp_dictoffset */
+    (initproc)FlacDecoder_init,/* tp_init */
+    0,                         /* tp_alloc */
+    FlacDecoder_new,       /* tp_new */
+};
+
+#else
+
+PyTypeObject decoders_FlacDecoderType = {
+    PyObject_HEAD_INIT(NULL)
+    0,                         /*ob_size*/
+    "decoders.FlacDecoder",    /*tp_name*/
+    sizeof(decoders_FlacDecoder), /*tp_basicsize*/
+    0,                         /*tp_itemsize*/
+    (destructor)FlacDecoder_dealloc, /*tp_dealloc*/
+    0,                         /*tp_print*/
+    0,                         /*tp_getattr*/
+    0,                         /*tp_setattr*/
+    0,                         /*tp_compare*/
+    0,                         /*tp_repr*/
+    0,                         /*tp_as_number*/
+    0,                         /*tp_as_sequence*/
+    0,                         /*tp_as_mapping*/
+    0,                         /*tp_hash */
+    0,                         /*tp_call*/
+    0,                         /*tp_str*/
+    0,                         /*tp_getattro*/
+    0,                         /*tp_setattro*/
+    0,                         /*tp_as_buffer*/
+    Py_TPFLAGS_DEFAULT | Py_TPFLAGS_BASETYPE, /*tp_flags*/
+    "FlacDecoder objects",     /* tp_doc */
+    0,		               /* tp_traverse */
+    0,		               /* tp_clear */
+    0,		               /* tp_richcompare */
+    0,		               /* tp_weaklistoffset */
+    0,		               /* tp_iter */
+    0,		               /* tp_iternext */
+    FlacDecoder_methods,       /* tp_methods */
+    0,                         /* tp_members */
+    FlacDecoder_getseters,     /* tp_getset */
+    0,                         /* tp_base */
+    0,                         /* tp_dict */
+    0,                         /* tp_descr_get */
+    0,                         /* tp_descr_set */
+    0,                         /* tp_dictoffset */
+    (initproc)FlacDecoder_init,/* tp_init */
+    0,                         /* tp_alloc */
+    FlacDecoder_new,           /* tp_new */
+};
+
+#endif
