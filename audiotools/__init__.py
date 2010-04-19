3d4b68dea2ba6943c7fb60b2453dc7596eaa221a
Brian Langenberger <bjl@usa.net>
2010-04-18 19:45:56 -0500

    Bump version number to 2.14 final.

--- audiotools/__init__.py
+++ audiotools/__init__.py
@@ -98,7 +98,7 @@ MUSICBRAINZ_PORT = config.getint_default("MusicBrainz","port",80)
 THUMBNAIL_FORMAT = config.get_default("Thumbnail","format","jpeg")
 THUMBNAIL_SIZE = config.getint_default("Thumbnail","size",150)
 
-VERSION = "2.14beta1"
+VERSION = "2.14"
 
 FILENAME_FORMAT = config.get_default(
     "Filenames","format",
