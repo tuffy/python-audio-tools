9b7b2543a7ad03788ea22779ba430b6f291c8167
Brian Langenberger <bjl@usa.net>
2010-05-26 09:36:37 -0500

    Bump version number to 2.15alpha3.
    Add a bunch of Python3-related TODO items.

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@
 #along with this program; if not, write to the Free Software
 #Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
-VERSION = '2.15alpha2'
+VERSION = '2.15alpha3'
 
 import sys
 
