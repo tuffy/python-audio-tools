2f7a2d02b44f795879d908c984d7435774c8ac1b
Brian Langenberger <bjl@usa.net>
2010-06-01 10:49:10 -0500

    Add preliminary ALAC LPC skeleton.
    I'll base it off the one used for FLAC but I'll limit my coefficient
    count to 4 or 8 since that's what iTunes seems to do.
    
    When in doubt, I'll try to build ALAC files indistinguishable
    from what iTunes generates.

--- src/encoders/alac.c
+++ src/encoders/alac.c
@@ -1,4 +1,5 @@
 #include "alac.h"
+#include "alac_lpc.h"
 
 /********************************************************
  Audio Tools, a module and set of tools for manipulating audio data
---
+++ src/encoders/alac_lpc.c
@@ -0,0 +1,21 @@
+#include "alac_lpc.h"
+
+void ALACEncoder_compute_best_lpc_coeffs(struct i_array *coeffs,
+					 int *shift_needed,
+
+					 struct alac_encoding_options *options,
+					 struct i_array *samples) {
+  /*window signal*/
+
+  /*compute autocorrelation*/
+
+  /*compute LP coefficients*/
+
+  /*estimate whether to use order 4 or 8*/
+
+  /*quantize coefficients*/
+
+  /*free temporary values*/
+
+  return;
+}
