89c650a10c7fe209edb13b9e67592be45bf294f4
Brian Langenberger <bjl@usa.net>
2010-08-28 15:36:27 -0500

    Add WavPack encoder block and sub-block header generation.
    Build a few more preliminary routines for block creation.

--- src/encoders/wavpack.c
+++ src/encoders/wavpack.c
@@ -29,7 +29,10 @@ encoders_encode_wavpack(PyObject *dummy,
     PyObject *pcmreader_obj;
     struct pcm_reader *reader;
 
+    struct wavpack_encoder_context context;
+
     struct ia_array samples;
+    ia_size_t i;
 
     int block_size;
     static char *kwlist[] = {"filename",
@@ -64,6 +67,11 @@ encoders_encode_wavpack(PyObject *dummy,
         return NULL;
     }
 
+    context.bits_per_sample = reader->bits_per_sample;
+    context.sample_rate = reader->sample_rate;
+    context.block_index = 0;
+    ia_init(&(context.block_offsets), 1);
+
     iaa_init(&samples, reader->channels, block_size);
 
     /*build frames until reader is empty
@@ -74,17 +82,24 @@ encoders_encode_wavpack(PyObject *dummy,
         goto error;
 
     while (samples.arrays[0].size > 0) {
-        WavPackEncoder_write_frame(stream, &samples, reader->channel_mask);
+        WavPackEncoder_write_frame(stream, &context,
+                                   &samples, reader->channel_mask);
 
         if (!pcmr_read(reader, block_size, &samples))
             goto error;
     }
 
     /*go back and set block header data as necessary*/
+    for (i = 0; i < context.block_offsets.size; i++) {
+        fseek(file, context.block_offsets.data[i] + 12, SEEK_SET);
+        stream->write_bits(stream, 32, context.block_index);
+    }
 
     /*close open file handles and deallocate temporary space*/
     pcmr_close(reader);
     bs_close(stream);
+    iaa_free(&samples);
+    ia_free(&(context.block_offsets));
 
     Py_INCREF(Py_None);
     return Py_None;
@@ -92,7 +107,20 @@ encoders_encode_wavpack(PyObject *dummy,
  error:
     pcmr_close(reader);
     bs_close(stream);
+    iaa_free(&samples);
+    ia_free(&(context.block_offsets));
+
     return NULL;
+}
+
+static int
+count_one_bits(int i) {
+    int bits;
+
+    for (bits = 0; i != 0; i >>= 1)
+        bits += (i & 1);
+
+    return bits;
 }
 
 static int
@@ -100,7 +128,7 @@ count_bits(int i) {
     int bits;
 
     for (bits = 0; i != 0; i >>= 1)
-        bits += (i & 1);
+        bits++;
 
     return bits;
 }
@@ -127,14 +155,14 @@ WavPackEncoder_channel_splits(struct i_array *counts,
     /*first, try to pull left/right channels out of the mask*/
     for (i = 0; channel_mask && masks[i]; i++) {
         if (channel_mask & masks[i]) {
-            channels = count_bits(masks[i]);
+            channels = count_one_bits(masks[i]);
             ia_append(counts, channels);
             channel_count -= channels;
             channel_mask ^= masks[i];
         }
     }
 
-    /*any leftover samples are sent out in separate blocks
+    /*any leftover channels are sent out in separate blocks
       (which may happen with a mask of 0)*/
     for (; channel_count > 0; channel_count--) {
         ia_append(counts, 1);
@@ -143,6 +171,7 @@ WavPackEncoder_channel_splits(struct i_array *counts,
 
 void
 WavPackEncoder_write_frame(Bitstream *bs,
+                           struct wavpack_encoder_context *context,
                            struct ia_array *samples,
                            long channel_mask) {
     struct i_array counts;
@@ -161,6 +190,7 @@ WavPackEncoder_write_frame(Bitstream *bs,
 
     for (i = current_channel = 0; i < counts.size; i++) {
         WavPackEncoder_write_block(bs,
+                                   context,
                                    &(samples->arrays[current_channel]),
                                    counts.data[i] == 2 ?
                                    &(samples->arrays[current_channel + 1]) :
@@ -171,19 +201,159 @@ WavPackEncoder_write_frame(Bitstream *bs,
         current_channel += counts.data[i];
     }
 
+    context->block_index += samples->arrays[0].size;
     ia_free(&counts);
+}
 
+ia_data_t
+WavPackEncoder_abs_maximum(ia_data_t sample, ia_data_t current_max) {
+    return MAX(abs(sample), current_max);
 }
 
 void
 WavPackEncoder_write_block(Bitstream *bs,
+                           struct wavpack_encoder_context *context,
                            struct i_array *channel_A,
                            struct i_array *channel_B,
                            int channel_count,
                            int first_block,
                            int last_block) {
+    struct wavpack_block_header block_header;
+    Bitstream *sub_blocks = bs_open_recorder();
+
+    /*this only works if Bitstream is a physical file*/
+    ia_append(&(context->block_offsets), ftell(bs->file));
+
+    /*initialize the WavPack block header fields*/
+
+    block_header.version = 0x407;
+    block_header.track_number = 0;
+    block_header.index_number = 0;
+    block_header.total_samples = 0; /*we won't know this in advance*/
+    block_header.block_index = context->block_index;
+    block_header.block_samples = channel_A->size;
+    block_header.bits_per_sample = context->bits_per_sample;
+    block_header.mono_output = channel_count == 1 ? 1 : 0;
+    block_header.hybrid_mode = 0;
+
+    block_header.hybrid_noise_shaping = 0;
+    block_header.floating_point_data = 0;
+    block_header.extended_size_integers = 0;
+    block_header.hybrid_parameters_control_bitrate = 0;
+    block_header.hybrid_noise_balanced = 0;
+    block_header.initial_block_in_sequence = first_block;
+    block_header.final_block_in_sequence = last_block;
+    block_header.left_shift = 0;
+
+    if (channel_count == 1)
+        block_header.maximum_data_magnitude = count_bits(
+                        ia_reduce(channel_A, 0, WavPackEncoder_abs_maximum));
+    else
+        block_header.maximum_data_magnitude = MAX(
+            count_bits(ia_reduce(channel_A, 0, WavPackEncoder_abs_maximum)),
+            count_bits(ia_reduce(channel_B, 0, WavPackEncoder_abs_maximum)));
+
+    block_header.sample_rate = context->sample_rate;
+    block_header.use_IIR = 0;
+
+    /*perform sub-block generation based on channel data
+      and encoding options*/
+    /*FIXME - add this*/
     fprintf(stderr, "writing block with channels = %d\n", channel_count);
     fprintf(stderr, "first block = %d\n", first_block);
     fprintf(stderr, "last block = %d\n", last_block);
-    return;
+
+    /*FIXME - set these to 0 for now*/
+    block_header.joint_stereo = 0;
+    block_header.cross_channel_decorrelation = 0;
+    block_header.false_stereo = 0;
+
+    /*update block header fields*/
+    block_header.block_size = sub_blocks->bits_written / 16;
+    block_header.crc = 0; /*FIXME - figure out how to calculate this*/
+
+    /*write block header*/
+    WavPackEncoder_write_block_header(bs, &block_header);
+
+    /*write sub-block data*/
+    bs_dump_records(bs, sub_blocks);
+
+    /*clear temporary space*/
+    bs_close(sub_blocks);
+}
+
+void
+WavPackEncoder_write_block_header(Bitstream *bs,
+                                  struct wavpack_block_header *header) {
+    bs->write_bits(bs, 32, 0x6B707677); /*block header*/
+    bs->write_bits(bs, 32, header->block_size);
+    bs->write_bits(bs, 16, header->version);
+    bs->write_bits(bs, 8,  header->track_number);
+    bs->write_bits(bs, 8,  header->index_number);
+    bs->write_bits(bs, 32, header->total_samples);
+    bs->write_bits(bs, 32, header->block_index);
+    bs->write_bits(bs, 32, header->block_samples);
+    switch (header->bits_per_sample) {
+    case 8:  bs->write_bits(bs, 2, 0); break;
+    case 16: bs->write_bits(bs, 2, 1); break;
+    case 24: bs->write_bits(bs, 2, 2); break;
+    case 32: bs->write_bits(bs, 2, 3); break;
+    }
+    bs->write_bits(bs, 1,  header->mono_output);
+    bs->write_bits(bs, 1,  header->hybrid_mode);
+    bs->write_bits(bs, 1,  header->joint_stereo);
+    bs->write_bits(bs, 1,  header->cross_channel_decorrelation);
+    bs->write_bits(bs, 1,  header->hybrid_noise_shaping);
+    bs->write_bits(bs, 1,  header->floating_point_data);
+    bs->write_bits(bs, 1,  header->extended_size_integers);
+    bs->write_bits(bs, 1,  header->hybrid_parameters_control_bitrate);
+    bs->write_bits(bs, 1,  header->hybrid_noise_balanced);
+    bs->write_bits(bs, 1,  header->initial_block_in_sequence);
+    bs->write_bits(bs, 1,  header->final_block_in_sequence);
+    bs->write_bits(bs, 5,  header->left_shift);
+    bs->write_bits(bs, 5,  header->maximum_data_magnitude);
+    switch (header->sample_rate) {
+    case 6000:   bs->write_bits(bs, 4, 0x0); break;
+    case 8000:   bs->write_bits(bs, 4, 0x1); break;
+    case 9600:   bs->write_bits(bs, 4, 0x2); break;
+    case 11025:  bs->write_bits(bs, 4, 0x3); break;
+    case 12000:  bs->write_bits(bs, 4, 0x4); break;
+    case 16000:  bs->write_bits(bs, 4, 0x5); break;
+    case 22050:  bs->write_bits(bs, 4, 0x6); break;
+    case 24000:  bs->write_bits(bs, 4, 0x7); break;
+    case 32000:  bs->write_bits(bs, 4, 0x8); break;
+    case 44100:  bs->write_bits(bs, 4, 0x9); break;
+    case 48000:  bs->write_bits(bs, 4, 0xA); break;
+    case 64000:  bs->write_bits(bs, 4, 0xB); break;
+    case 88200:  bs->write_bits(bs, 4, 0xC); break;
+    case 96000:  bs->write_bits(bs, 4, 0xD); break;
+    case 192000: bs->write_bits(bs, 4, 0xE); break;
+    default:     bs->write_bits(bs, 4, 0xF); break;
+    }
+    bs->write_bits(bs, 2,  0);
+    bs->write_bits(bs, 1,  header->use_IIR);
+    bs->write_bits(bs, 1,  header->false_stereo);
+    bs->write_bits(bs, 1,  0);
+    bs->write_bits(bs, 32, header->crc);
+}
+
+void
+WavPackEncoder_write_subblock_header(Bitstream *bs,
+                                     wv_metadata_function metadata_function,
+                                     uint8_t nondecoder_data,
+                                     uint32_t block_size) {
+    bs->write_bits(bs, 5, metadata_function);
+    bs->write_bits(bs, 1, nondecoder_data);
+    bs->write_bits(bs, 1, block_size % 2);
+
+    /*convert block_size bytes to WavPack's 16-bit block size field*/
+    block_size = (block_size / 2) + (block_size % 2);
+
+    if (block_size > 0xFF) {
+        bs->write_bits(bs, 1,  1);
+        bs->write_bits(bs, 24, block_size);
+    } else {
+        bs->write_bits(bs, 1,  0);
+        bs->write_bits(bs, 8,  block_size);
+    }
 }
--- src/encoders/wavpack.h
+++ src/encoders/wavpack.h
@@ -24,8 +24,56 @@
 
 typedef enum {OK, ERROR} status;
 
+typedef enum {WV_DECORR_TERMS      = 0x2,
+              WV_DECORR_WEIGHTS    = 0x3,
+              WV_DECORR_SAMPLES    = 0x4,
+              WV_ENTROPY_VARIABLES = 0x5,
+              WV_INT32_INFO        = 0x9,
+              WV_BITSTREAM         = 0xA} wv_metadata_function;
+
+struct wavpack_encoder_context {
+    uint8_t bits_per_sample;
+    uint32_t sample_rate;
+    uint32_t block_index;
+    struct i_array block_offsets;
+};
+
+struct wavpack_block_header {
+    /*block ID                                   32 bits*/
+    uint32_t block_size;                       /*32 bits*/
+    uint16_t version;                          /*16 bits*/
+    uint8_t track_number;                      /*8 bits*/
+    uint8_t index_number;                      /*8 bits*/
+    uint32_t total_samples;                    /*32 bits*/
+    uint32_t block_index;                      /*32 bits*/
+    uint32_t block_samples;                    /*32 bits*/
+
+    uint8_t bits_per_sample;                   /*2 bits*/
+    uint8_t mono_output;                       /*1 bit*/
+    uint8_t hybrid_mode;                       /*1 bit*/
+    uint8_t joint_stereo;                      /*1 bit*/
+    uint8_t cross_channel_decorrelation;       /*1 bit*/
+    uint8_t hybrid_noise_shaping;              /*1 bit*/
+    uint8_t floating_point_data;               /*1 bit*/
+    uint8_t extended_size_integers;            /*1 bit*/
+    uint8_t hybrid_parameters_control_bitrate; /*1 bit*/
+    uint8_t hybrid_noise_balanced;             /*1 bit*/
+    uint8_t initial_block_in_sequence;         /*1 bit*/
+    uint8_t final_block_in_sequence;           /*1 bit*/
+    uint8_t left_shift;                        /*5 bits*/
+    uint8_t maximum_data_magnitude;            /*5 bits*/
+    uint32_t sample_rate;                      /*4 bits*/
+    /*reserved                                   2 bits*/
+    uint8_t use_IIR;                           /*1 bit*/
+    uint8_t false_stereo;                      /*1 bit*/
+    /*reserved                                   1 bit*/
+
+    uint32_t crc;                              /*32 bits*/
+};
+
 void
 WavPackEncoder_write_frame(Bitstream *bs,
+                           struct wavpack_encoder_context *context,
                            struct ia_array *samples,
                            long channel_mask);
 
@@ -39,8 +87,29 @@ WavPackEncoder_channel_splits(struct i_array *counts,
 
 void
 WavPackEncoder_write_block(Bitstream *bs,
+                           struct wavpack_encoder_context *context,
                            struct i_array *channel_A,
                            struct i_array *channel_B,
                            int channel_count,
                            int first_block,
                            int last_block);
+
+ia_data_t
+WavPackEncoder_abs_maximum(ia_data_t sample, ia_data_t current_max);
+
+void
+WavPackEncoder_write_block_header(Bitstream *bs,
+                                  struct wavpack_block_header *header);
+
+/*nondecoder data should be 0 or 1.
+  block_size is in bytes.
+  This will convert to WavPack's size value and set
+  "actual size 1 less" as needed.*/
+void
+WavPackEncoder_write_subblock_header(Bitstream *bs,
+                                     wv_metadata_function metadata_function,
+                                     uint8_t nondecoder_data,
+                                     uint32_t block_size);
+
+
+
