fb8dfcad6e2191898cfb81a113201d411e608f88
Brian Langenberger <bjl@usa.net>
2010-10-18 14:48:15 -0500

    Bump version to 2.16beta1
    Though a few bugs and TODO items remain, all new features are on hold
    until this version is finalized and released.

--- audiotools/__init__.py
+++ audiotools/__init__.py
@@ -107,7 +107,7 @@ MUSICBRAINZ_PORT = config.getint_default("MusicBrainz", "port", 80)
 THUMBNAIL_FORMAT = config.get_default("Thumbnail", "format", "jpeg")
 THUMBNAIL_SIZE = config.getint_default("Thumbnail", "size", 150)
 
-VERSION = "2.16alpha4"
+VERSION = "2.16beta1"
 
 FILENAME_FORMAT = config.get_default(
     "Filenames", "format",
