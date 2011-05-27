ce946cb2d95a0fc237e26d240bea61ec0ae3592e
Brian Langenberger <bjl@usa.net>
2011-05-27 13:21:51 -0500

    Significant updates to the Vorbis codebook documentation.
    Add color support to the bitstream figure generator
    which will (hopefully) make the process easier to follow.
    
    Trying to make this comprehensible reflects on the difficulty in
    illustrating binary file formats.
    Bit diagrams/subdiagrams are easy to visualize, but struggle with
    any sort of nontrivial branching logic.
    Flowcharts are easy to follow and transcribe into source code,
    but difficult to visualize without some accompanying examples
    (and they don't scale well either).
    
    Just one more thing I'll need to improve.

--- docs/reference/bitdiagram.py
+++ docs/reference/bitdiagram.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import sys
+import re
 
 try:
     from reportlab.pdfgen import canvas
@@ -16,25 +17,88 @@ except ImportError:
 (NE,NW,SE,SW) = range(4)
 ROW_HEIGHT = 22
 
+class RGB_Color:
+    RGB = re.compile(r'^#([0-9A-Fa-f]{2})([0-9A-Fa-f]{2})([0-9A-Fa-f]{2})$')
+    RGBA = re.compile(r'^#([0-9A-Fa-f]{2})([0-9A-Fa-f]{2})([0-9A-Fa-f]{2})' +
+                      r'([0-9A-Fa-f]{2})$')
+
+    COLOR_TABLE = {u"red":(1.0, 0.0, 0.0),
+                   u"orange":(1.0, 0.4, 0.0),
+                   u"yellow":(1.0, 1.0, 0.0),
+                   u"green":(0.0, 1.0, 0.0),
+                   u"blue":(0.0, 0.0, 1.0),
+                   u"aqua":(0.0, 1.0, 1.0),
+                   u"black":(0.0, 0.0, 0.0),
+                   u"fuchsia":(1.0, 0.0, 1.0),
+                   u"gray":(0.5, 0.5, 0.5),
+                   u"lime":(0.0, 1.0, 0.0),
+                   u"maroon":(0.5, 0.0, 0.0),
+                   u"navy":(0.0, 0.0, 0.5),
+                   u"olive":(0.5, 0.5, 0.0),
+                   u"purple":(0.5, 0.0, 0.5),
+                   u"silver":(0.75, 0.75, 0.75),
+                   u"teal":(0.0, 0.5, 0.5),
+                   u"white":(1.0, 1.0, 1.0)}
+
+    def __init__(self, red, green, blue, alpha=None):
+        """all should be floats between 0.0 and 1.0"""
+
+        self.red = red
+        self.green = green
+        self.blue = blue
+        self.alpha = alpha
+
+    @classmethod
+    def from_string(cls, s):
+        if (s in cls.COLOR_TABLE):
+            (r, g, b) = cls.COLOR_TABLE[s]
+            return cls(red=r, green=g, blue=b, alpha=1.0)
+        else:
+            rgb = cls.RGB.match(s)
+            if (rgb is not None):
+                return cls(red=int(rgb.group(1), 16) / 255.0,
+                           green=int(rgb.group(2), 16) / 255.0,
+                           blue=int(rgb.group(3), 16) / 255.0)
+            else:
+                rgba = cls.RGBA.match(s)
+                if (rgba is not None):
+                    return cls(red=int(rgba.group(1), 16) / 255.0,
+                               green=int(rgba.group(2), 16) / 255.0,
+                               blue=int(rgba.group(3), 16) / 255.0,
+                               alpha=int(rgba.group(4), 16) / 255.0)
+                else:
+                    raise ValueError("invalid color string %s" % (repr(s)))
+
+
+
+
 class Chunk:
-    #text should be the chunk's text Unicode string, or None
-    #width is size of the chunk as a floating point percentage
-    #chunk_id is a unique text string, or None
-    #style is one of the style enumerations such as SOLID, DASHED, etc.
-    def __init__(self, text = None, start_size = None, end_size = None,
-                 width = 1.0, chunk_id = None, style = SOLID):
+    def __init__(self, text=None, start_size=None, end_size=None,
+                 width=1.0, chunk_id=None, style=SOLID,
+                 text_color=None, border_color=None, background_color=None):
+        """text should be the chunk's text Unicode string, or None
+start_size and end_size should be Unicode strings, or None
+width is size of the chunk as a floating point percentage
+chunk_id is a unique text string, or None
+style is one of the style enumerations such as SOLID, DASHED, etc."""
         self.text = text
         self.width = width
         self.id = chunk_id
         self.style = style
+
         if (start_size is not None):
             self.start_size = unicode(start_size)
         else:
             self.start_size = None
+
         if (end_size is not None):
             self.end_size = unicode(end_size)
         else:
             self.end_size = None
+
+        self.text_color = text_color
+        self.border_color = border_color
+        self.background_color = background_color
 
         #the chunk's location in the PDF, in x,y point pairs
         self.ne = self.nw = self.se = self.sw = (0,0)
@@ -83,8 +147,13 @@ class Chunk:
              self.width,self.id,self.style)
 
     def to_pdf(self, pdf):
-        pdf.setStrokeColorRGB(0,0,0)
-        pdf.setFillColorRGB(0,0,0)
+        if (self.border_color is None):
+            pdf.setStrokeColorRGB(0,0,0,1)
+        else:
+            pdf.setStrokeColorRGB(r=self.border_color.red,
+                                  g=self.border_color.green,
+                                  b=self.border_color.blue,
+                                  alpha=self.border_color.alpha)
 
         if (self.style is not BLANK):
             if (self.style == SOLID):
@@ -93,7 +162,33 @@ class Chunk:
                 pdf.setDash(6,6)
             elif (self.style == DOTTED):
                 pdf.setDash(1,6)
-            pdf.rect(self.sw[0],self.sw[1],self.pt_width(),self.pt_height())
+
+            if (self.background_color is None):
+                pdf.rect(x=self.sw[0],
+                         y=self.sw[1],
+                         width=self.pt_width(),
+                         height=self.pt_height(),
+                         stroke=1,
+                         fill=0)
+            else:
+                pdf.setFillColorRGB(r=self.background_color.red,
+                                g=self.background_color.green,
+                                b=self.background_color.blue,
+                                alpha=self.background_color.alpha)
+                pdf.rect(x=self.sw[0],
+                         y=self.sw[1],
+                         width=self.pt_width(),
+                         height=self.pt_height(),
+                         stroke=1,
+                         fill=1)
+
+        if (self.text_color is None):
+            pdf.setFillColorRGB(0,0,0,1)
+        else:
+            pdf.setFillColorRGB(r=self.text_color.red,
+                                g=self.text_color.green,
+                                b=self.text_color.blue,
+                                alpha=self.text_color.alpha)
 
         if (self.text is not None):
             pdf.setFont("DejaVu",10)
@@ -176,12 +271,13 @@ class Line:
     def __init__(self,
                  start_chunk, start_corner,
                  end_chunk, end_corner,
-                 style=SOLID):
+                 style=SOLID, color=None):
         self.start_chunk = start_chunk
         self.start_corner = start_corner
         self.end_chunk = end_chunk
         self.end_corner = end_corner
         self.style = style
+        self.color = color
 
     def render(self, pdf):
         if (self.style is not BLANK):
@@ -191,7 +287,13 @@ class Line:
                 pdf.setDash(6,6)
             elif (self.style == DOTTED):
                 pdf.setDash(1,6)
-            pdf.setStrokeColorRGB(0,0,0)
+            if (self.color is None):
+                pdf.setStrokeColorRGB(0,0,0)
+            else:
+                pdf.setStrokeColorRGB(r=self.color.red,
+                                      g=self.color.green,
+                                      b=self.color.blue,
+                                      alpha=self.color.alpha)
             (start_x,start_y) = self.start_chunk.get_corner(self.start_corner)
             (end_x,end_y) = self.end_chunk.get_corner(self.end_corner)
             pdf.line(start_x,start_y,end_x,end_y)
@@ -221,10 +323,13 @@ class ChunkTable:
 
     def add_line(self, start_id, start_corner,
                  end_id, end_corner,
-                 style=SOLID):
-        self.lines.append(Line(self.chunk_map[start_id],start_corner,
-                               self.chunk_map[end_id],end_corner,
-                               style))
+                 style=SOLID, color=None):
+        self.lines.append(Line(start_chunk=self.chunk_map[start_id],
+                               start_corner=start_corner,
+                               end_chunk=self.chunk_map[end_id],
+                               end_corner=end_corner,
+                               style=style,
+                               color=color))
 
     #given a width value (in points) and filename string,
     #render all the lines and chunks as a PDF file
@@ -307,21 +412,31 @@ def parse_xml(xml_filename):
                     chunk_args = {}
                     if (len(col.childNodes) > 0):
                         chunk_args["text"] = col.childNodes[0].data
-                    if (col.hasAttribute("start")):
-                        chunk_args["start_size"] = col.getAttribute("start")
-                    if (col.hasAttribute("end")):
-                        chunk_args["end_size"] = col.getAttribute("end")
-                    if (col.hasAttribute("width")):
-                        chunk_args["width"] = float(col.getAttribute("width"))
-                    if (col.hasAttribute("id")):
-                        chunk_args["chunk_id"] = col.getAttribute("id")
-                    if (col.hasAttribute("style")):
-                        chunk_args["style"] = STYLE_MAP[col.getAttribute("style")]
+                    if (col.hasAttribute(u"start")):
+                        chunk_args["start_size"] = col.getAttribute(u"start")
+                    if (col.hasAttribute(u"end")):
+                        chunk_args["end_size"] = col.getAttribute(u"end")
+                    if (col.hasAttribute(u"width")):
+                        chunk_args["width"] = float(col.getAttribute(u"width"))
+                    if (col.hasAttribute(u"id")):
+                        chunk_args["chunk_id"] = col.getAttribute(u"id")
+                    if (col.hasAttribute(u"style")):
+                        chunk_args["style"] = STYLE_MAP[col.getAttribute(u"style")]
+                    if (col.hasAttribute(u"text-color")):
+                        chunk_args["text_color"] = RGB_Color.from_string(
+                            col.getAttribute(u"text-color"))
+                    if (col.hasAttribute(u"border-color")):
+                        chunk_args["border_color"] = RGB_Color.from_string(
+                            col.getAttribute(u"border-color"))
+                    if (col.hasAttribute(u"background-color")):
+                        chunk_args["background_color"] = RGB_Color.from_string(
+                            col.getAttribute(u"background-color"))
+
                     columns.append(Chunk(**chunk_args))
                 elif (col.nodeName == u'blank'):
                     chunk_args = {}
                     if (col.hasAttribute("width")):
-                        chunk_args["width"] = float(col.getAttribute("width"))
+                        chunk_args["width"] = float(col.getAttribute(u"width"))
                     columns.append(BlankChunk(**chunk_args))
 
             table.add_row(*columns)
@@ -329,18 +444,28 @@ def parse_xml(xml_filename):
             if (part.hasAttribute("height")):
                 table.add_spacer(int(
                         round(ROW_HEIGHT *
-                              float(part.getAttribute("height")))))
+                              float(part.getAttribute(u"height")))))
             else:
                 table.add_spacer(ROW_HEIGHT)
         elif (part.nodeName == u"line"):
             start = part.getElementsByTagName(u"start")[0]
             end = part.getElementsByTagName(u"end")[0]
+            if (part.hasAttribute(u"style")):
+                style = part.getAttribute(u"style")
+            else:
+                style= u"solid"
+            if (part.hasAttribute(u"color")):
+                color = RGB_Color.from_string(part.getAttribute(u"color"))
+            else:
+                color = None
 
-            table.add_line(start.getAttribute("id"),
-                           CORNER_MAP[start.getAttribute("corner")],
-                           end.getAttribute("id"),
-                           CORNER_MAP[end.getAttribute("corner")],
-                           STYLE_MAP[part.getAttribute("style")])
+            table.add_line(
+                start_id=start.getAttribute(u"id"),
+                start_corner=CORNER_MAP[start.getAttribute(u"corner")],
+                end_id=end.getAttribute(u"id"),
+                end_corner=CORNER_MAP[end.getAttribute(u"corner")],
+                style=STYLE_MAP[style],
+                color=color)
 
     return table
 
