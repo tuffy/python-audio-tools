6a6f5c5bbcbc9d0c483828a3d70258dfd68280d9
Brian Langenberger <bjl@usa.net>
2011-11-23 14:51:34 -0600

    Add WavPack residual decoding to overhauled documentation.
    It's still a complex pile of steps,
    but at least it fits on two opposing pages now.

--- audiotools/py_decoders/wavpack.py
+++ audiotools/py_decoders/wavpack.py
@@ -38,6 +38,10 @@ class WavPackDecoder:
                         self.parse_decorrelation_terms(sub_block)
                     elif (sub_block.metadata_function == 3):
                         self.parse_decorrelation_weights(header, sub_block)
+                    elif (sub_block.metadata_function == 4):
+                        self.parse_decorrelation_samples(header, sub_block)
+                    elif (sub_block.metadata_function == 5):
+                        self.parse_entropy_variables(header, sub_block)
                     else:
                         #FIXME - parser decoding parameters from sub block
                         print sub_block.metadata_function
@@ -61,6 +65,11 @@ class WavPackDecoder:
     def reset_decoding_parameters(self):
         self.decorrelation_terms = []
         self.decorrelation_deltas = []
+        self.channel_A_weights = []
+        self.channel_B_weights = []
+
+        self.decorrelation_terms_read = False
+        self.decorrelation_weights_read = False
 
     def parse_decorrelation_terms(self, sub_block):
         if (sub_block.actual_size_1_less == 0):
@@ -85,7 +94,11 @@ class WavPackDecoder:
         self.decorrelation_terms.reverse()
         self.decorrelation_deltas.reverse()
 
+        self.decorrelation_terms_read = True
+
     def parse_decorrelation_weights(self, block_header, sub_block):
+        assert(self.decorrelation_terms_read)
+
         if (sub_block.actual_size_1_less == 0):
             weight_count = sub_block.sub_block_size * 2
         else:
@@ -124,7 +137,128 @@ class WavPackDecoder:
                 self.channel_B_weights.append(0)
             self.channel_A_weights.reverse()
 
-        print repr(self.channel_A_weights),repr(self.channel_B_weights)
+        self.decorrelation_weights_read = True
+
+    def parse_decorrelation_samples(self, header, sub_block):
+        assert(self.decorrelation_terms_read)
+        sub_block_bytes = sub_block.data_size()
+
+        channel_A_samples = []
+        channel_B_samples = []
+        if (header.mono_output == 0):
+            for term in reversed(self.decorrelation_terms):
+                if ((17 <= term) and (term <= 18)):
+                    if (sub_block_bytes >= 8):
+                        channel_A_samples.append([read_exp2(sub_block.data),
+                                                  read_exp2(sub_block.data)])
+                        channel_B_samples.append([read_exp2(sub_block.data),
+                                                  read_exp2(sub_block.data)])
+                        sub_block_bytes -= 8
+                    else:
+                        channel_A_samples.append([0, 0])
+                        channel_B_samples.append([0, 0])
+                        sub_block_bytes = 0
+                elif ((1 <= term) and (term <= 8)):
+                    if (sub_block_bytes >= (term * 4)):
+                        channel_A_samples.append([read_exp2(sub_block.data)
+                                                  for i in xrange(term)])
+                        channel_B_samples.append([read_exp2(sub_block.data)
+                                                  for i in xrange(term)])
+                        sub_block_bytes -= (term * 4)
+                    else:
+                        channel_A_samples.append([0 for i in xrange(term)])
+                        channel_B_samples.append([0 for i in xrange(term)])
+                        sub_block_bytes = 0
+                elif ((-3 <= term) and (term <= -1)):
+                    if (sub_block_bytes >= 4):
+                        channel_A_samples.append([read_exp2(sub_block.data)])
+                        channel_B_samples.append([read_exp2(sub_block.data)])
+                        sub_block_bytes -= 4
+                    else:
+                        channel_A_samples.append([0])
+                        channel_B_samples.append([0])
+                        sub_block_bytes = 0
+                else:
+                    raise ValueError("invalid decorrelation term")
+        else:
+            for term in reversed(self.decorrelation_terms):
+                if ((17 <= term) and (term <= 18)):
+                    if (sub_block_bytes >= 4):
+                        channel_A_samples.append([read_exp2(sub_block.data),
+                                                  read_exp2(sub_block.data)])
+                        sub_block_bytes -= 4
+                    else:
+                        channel_A_samples.append([0, 0])
+                        sub_block_bytes = 0
+                elif ((1 <= term) and (term <= 8)):
+                    if (sub_block_bytes >= (term * 2)):
+                        channel_A_samples.append([read_exp2(sub_block.data)
+                                                  for i in xrange(term)])
+                        sub_block_bytes -= (term * 2)
+                    else:
+                        channel_A_samples.append([0 for i in xrange(term)])
+                        sub_block_bytes = 0
+                else:
+                    raise ValueError("invalid decorrelation term")
+
+        channel_A_samples.reverse()
+        channel_B_samples.reverse()
+
+        return (channel_A_samples, channel_B_samples)
+
+    def parse_entropy_variables(self, header, sub_block):
+        median0 = [read_exp2(sub_block.data) for i in xrange(3)]
+        if (header.mono_output == 0):
+            median1 = [read_exp2(sub_block.data) for i in xrange(3)]
+        else:
+            median1 = [0 for i in xrange(3)]
+
+        return (median0, median1)
+
+EXP2 = [0x100, 0x101, 0x101, 0x102, 0x103, 0x103, 0x104, 0x105,
+        0x106, 0x106, 0x107, 0x108, 0x108, 0x109, 0x10a, 0x10b,
+        0x10b, 0x10c, 0x10d, 0x10e, 0x10e, 0x10f, 0x110, 0x110,
+        0x111, 0x112, 0x113, 0x113, 0x114, 0x115, 0x116, 0x116,
+        0x117, 0x118, 0x119, 0x119, 0x11a, 0x11b, 0x11c, 0x11d,
+        0x11d, 0x11e, 0x11f, 0x120, 0x120, 0x121, 0x122, 0x123,
+        0x124, 0x124, 0x125, 0x126, 0x127, 0x128, 0x128, 0x129,
+        0x12a, 0x12b, 0x12c, 0x12c, 0x12d, 0x12e, 0x12f, 0x130,
+        0x130, 0x131, 0x132, 0x133, 0x134, 0x135, 0x135, 0x136,
+        0x137, 0x138, 0x139, 0x13a, 0x13a, 0x13b, 0x13c, 0x13d,
+        0x13e, 0x13f, 0x140, 0x141, 0x141, 0x142, 0x143, 0x144,
+        0x145, 0x146, 0x147, 0x148, 0x148, 0x149, 0x14a, 0x14b,
+        0x14c, 0x14d, 0x14e, 0x14f, 0x150, 0x151, 0x151, 0x152,
+        0x153, 0x154, 0x155, 0x156, 0x157, 0x158, 0x159, 0x15a,
+        0x15b, 0x15c, 0x15d, 0x15e, 0x15e, 0x15f, 0x160, 0x161,
+        0x162, 0x163, 0x164, 0x165, 0x166, 0x167, 0x168, 0x169,
+        0x16a, 0x16b, 0x16c, 0x16d, 0x16e, 0x16f, 0x170, 0x171,
+        0x172, 0x173, 0x174, 0x175, 0x176, 0x177, 0x178, 0x179,
+        0x17a, 0x17b, 0x17c, 0x17d, 0x17e, 0x17f, 0x180, 0x181,
+        0x182, 0x183, 0x184, 0x185, 0x187, 0x188, 0x189, 0x18a,
+        0x18b, 0x18c, 0x18d, 0x18e, 0x18f, 0x190, 0x191, 0x192,
+        0x193, 0x195, 0x196, 0x197, 0x198, 0x199, 0x19a, 0x19b,
+        0x19c, 0x19d, 0x19f, 0x1a0, 0x1a1, 0x1a2, 0x1a3, 0x1a4,
+        0x1a5, 0x1a6, 0x1a8, 0x1a9, 0x1aa, 0x1ab, 0x1ac, 0x1ad,
+        0x1af, 0x1b0, 0x1b1, 0x1b2, 0x1b3, 0x1b4, 0x1b6, 0x1b7,
+        0x1b8, 0x1b9, 0x1ba, 0x1bc, 0x1bd, 0x1be, 0x1bf, 0x1c0,
+        0x1c2, 0x1c3, 0x1c4, 0x1c5, 0x1c6, 0x1c8, 0x1c9, 0x1ca,
+        0x1cb, 0x1cd, 0x1ce, 0x1cf, 0x1d0, 0x1d2, 0x1d3, 0x1d4,
+        0x1d6, 0x1d7, 0x1d8, 0x1d9, 0x1db, 0x1dc, 0x1dd, 0x1de,
+        0x1e0, 0x1e1, 0x1e2, 0x1e4, 0x1e5, 0x1e6, 0x1e8, 0x1e9,
+        0x1ea, 0x1ec, 0x1ed, 0x1ee, 0x1f0, 0x1f1, 0x1f2, 0x1f4,
+        0x1f5, 0x1f6, 0x1f8, 0x1f9, 0x1fa, 0x1fc, 0x1fd, 0x1ff]
+
+def read_exp2(reader):
+    value = reader.read_signed(16)
+    if ((-32768 <= value) and (value < -2304)):
+        return -(EXP2[-value & 0xFF] << ((-value >> 8) - 9))
+    elif ((-2304 <= value) and (value < 0)):
+        return -(EXP2[-value & 0xFF] >> (9 - (-value >> 8)))
+    elif ((0 <= value) and (value <= 2304)):
+        return EXP2[value & 0xFF] >> (9 - (value >> 8))
+    elif ((2304 < value) and (value <= 32767)):
+        return EXP2[value & 0xFF] << ((value >> 8) - 9)
+
 
 class Block_Header:
     def __init__(self,
@@ -215,7 +349,7 @@ class Sub_Block:
         if (self.actual_size_1_less):
             return self.sub_block_size * 2 - 1
         else:
-            return self.sub_block_size * 2 - 1
+            return self.sub_block_size * 2
 
     @classmethod
     def read(cls, reader):
