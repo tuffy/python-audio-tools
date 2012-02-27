32b31b9800e61126cbb1ff96c83e7a5dc15d2e56
Brian Langenberger <bjl@usa.net>
2012-02-27 15:15:14 -0600

    Split CPPM routines into Python/non-Python segments.

--- src/decoders/cppm.h
+++ src/decoders/cppm.h
@@ -1,6 +1,5 @@
 #ifndef INC_CPPM
 #define INC_CPPM
-#include <Python.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
@@ -8,118 +7,15 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdint.h>
 
-/********************************************************
- Audio Tools, a module and set of tools for manipulating audio data
- Copyright (C) 2007-2012  Brian Langenberger
-
- This program is free software; you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation; either version 2 of the License, or
- (at your option) any later version.
-
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
-
- You should have received a copy of the GNU General Public License
- along with this program; if not, write to the Free Software
- Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
-*******************************************************/
+/* The block size of a DVD. */
+const static unsigned DVDCPXM_BLOCK_SIZE = 2048;
 
 struct cppm_decoder {
     int        media_type;     /*read from DVD side data*/
     uint64_t   media_key;      /*read from AUDIO_TS/DVDAUDIO.MKB file*/
     uint64_t   id_album_media; /*pulled from DVD side data*/
-};
-
-typedef struct {
-    PyObject_HEAD
-
-    struct cppm_decoder decoder;
-} decoders_CPPMDecoder;
-
-/*the CPPMDecoder.__init__() method*/
-int
-CPPMDecoder_init(decoders_CPPMDecoder *self,
-                 PyObject *args, PyObject *kwds);
-
-static PyObject*
-CPPMDecoder_new(PyTypeObject *type,
-                PyObject *args, PyObject *kwds);
-
-void
-CPPMDecoder_dealloc(decoders_CPPMDecoder *self);
-
-static PyObject*
-CPPMDecoder_media_type(decoders_CPPMDecoder *self, void *closure);
-
-static PyObject*
-CPPMDecoder_media_key(decoders_CPPMDecoder *self, void *closure);
-
-static PyObject*
-CPPMDecoder_id_album_media(decoders_CPPMDecoder *self, void *closure);
-
-PyGetSetDef CPPMDecoder_getseters[] = {
-    {"media_type",
-     (getter)CPPMDecoder_media_type, NULL, "media_type", NULL},
-    {"media_key",
-     (getter)CPPMDecoder_media_key, NULL, "media_key", NULL},
-    {"id_album_media",
-     (getter)CPPMDecoder_id_album_media, NULL, "id_album_media", NULL},
-    {NULL}
-};
-
-static PyObject*
-CPPMDecoder_decode(decoders_CPPMDecoder *self, PyObject *args);
-
-PyMethodDef CPPMDecoder_methods[] = {
-    {"decode", (PyCFunction)CPPMDecoder_decode,
-     METH_VARARGS, "Decodes one or more 2048 byte blocks"},
-    {NULL}
-};
-
-PyTypeObject decoders_CPPMDecoderType = {
-    PyObject_HEAD_INIT(NULL)
-    0,                         /*ob_size*/
-    "decoders.CPPMDecoder",    /*tp_name*/
-    sizeof(decoders_CPPMDecoder), /*tp_basicsize*/
-    0,                         /*tp_itemsize*/
-    (destructor)CPPMDecoder_dealloc, /*tp_dealloc*/
-    0,                         /*tp_print*/
-    0,                         /*tp_getattr*/
-    0,                         /*tp_setattr*/
-    0,                         /*tp_compare*/
-    0,                         /*tp_repr*/
-    0,                         /*tp_as_number*/
-    0,                         /*tp_as_sequence*/
-    0,                         /*tp_as_mapping*/
-    0,                         /*tp_hash */
-    0,                         /*tp_call*/
-    0,                         /*tp_str*/
-    0,                         /*tp_getattro*/
-    0,                         /*tp_setattro*/
-    0,                         /*tp_as_buffer*/
-    Py_TPFLAGS_DEFAULT | Py_TPFLAGS_BASETYPE, /*tp_flags*/
-    "CPPMDecoder objects", /* tp_doc */
-    0,                     /* tp_traverse */
-    0,                     /* tp_clear */
-    0,                     /* tp_richcompare */
-    0,                     /* tp_weaklistoffset */
-    0,                     /* tp_iter */
-    0,                     /* tp_iternext */
-    CPPMDecoder_methods,       /* tp_methods */
-    0,                         /* tp_members */
-    CPPMDecoder_getseters,     /* tp_getset */
-    0,                         /* tp_base */
-    0,                         /* tp_dict */
-    0,                         /* tp_descr_get */
-    0,                         /* tp_descr_set */
-    0,                         /* tp_dictoffset */
-    (initproc)CPPMDecoder_init,/* tp_init */
-    0,                         /* tp_alloc */
-    CPPMDecoder_new,           /* tp_new */
 };
 
 typedef enum {COPYRIGHT_PROTECTION_NONE = 0,
