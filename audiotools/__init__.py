72da23f975e0b31b01c0c88bd5e90cda131228dc
Brian Langenberger <bjl@usa.net>
2012-03-11 15:29:32 -0500

    Bump version to 2.18beta1
    There's still a number of bugfixes and updated unit tests needed
    before I can call it 2.18 final.  But all the major features are done
    for this development cycle.

--- audiotools/__init__.py
+++ audiotools/__init__.py
@@ -139,7 +139,7 @@ MUSICBRAINZ_PORT = config.getint_default("MusicBrainz", "port", 80)
 THUMBNAIL_FORMAT = config.get_default("Thumbnail", "format", "jpeg")
 THUMBNAIL_SIZE = config.getint_default("Thumbnail", "size", 150)
 
-VERSION = "2.18alpha4"
+VERSION = "2.18beta1"
 
 FILENAME_FORMAT = config.get_default(
     "Filenames", "format",
