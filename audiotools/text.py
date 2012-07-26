57b1150953331d307d80ec1412a720154b7960e6
Brian Langenberger <bjl@usa.net>
2012-07-26 15:08:34 -0500

    Overhaul most utilities for new text interface.
    Another pass through the utilities will be needed to avoid
    importing dozens of constants at the start of the file
    in favor of importing them as needed.

--- audiotools/text.py
+++ audiotools/text.py
@@ -19,6 +19,158 @@
 
 """a text strings module"""
 
+#Utility usage
+USAGE_CD2TRACK = \
+    u"%prog [options] [track #] [track #] ..."
+
+USAGE_CDINFO = \
+    u"%prog [options]"
+
+USAGE_CDPLAY = \
+    u"%prog [track 1] [track 2] ..."
+
+USAGE_TRACKPLAY = \
+    u"%prog [track 1] [track 2] ..."
+
+USAGE_COVERDUMP = \
+    u"%prog [-d directory] <track>"
+
+USAGE_COVERVIEW = \
+    u"%prog [OPTIONS] [track]"
+
+USAGE_DVDA2TRACK = \
+    u"%prog [options] [track #] [track #] ..."
+
+USAGE_DVDAINFO = \
+    u"%prog [options]"
+
+USAGE_TRACK2CD = \
+    u"%prog [options] <track 1> [track 2] ..."
+
+USAGE_TRACK2TRACK = \
+    u"%prog [options] <track 1> [track 2] ..."
+
+USAGE_TRACKCAT = \
+    u"%prog [options] [-o output] <track 1> [track 2] ..."
+
+USAGE_TRACKCMP = \
+    u"%prog <file 1> <file 2>"
+
+USAGE_TRACKCMP_CDIMAGE = \
+    u"<CD image> <track 1> <track 2> ..."
+
+USAGE_TRACKCMP_FILES = \
+    u"<track 1> <track 2>"
+
+USAGE_TRACKSPLIT = \
+    u"%prog [options] [-d directory] <track>"
+
+USAGE_TRACKINFO = \
+    u"%prog [options] <track 1> [track 2] ..."
+
+USAGE_TRACKLENGTH = \
+    u"%prog <track 1> [track 2] ..."
+
+#Utility Options
+OPT_VERBOSE = \
+    u"the verbosity level to execute at"
+OPT_TYPE = \
+    u"the type of audio track to create"
+OPT_QUALITY = \
+    u"the quality to store audio tracks at"
+OPT_DIR = \
+    u"the directory to store new audio tracks"
+OPT_DIR_IMAGES = \
+    u"the directory to store extracted images"
+OPT_FORMAT = \
+    u"the format string for new filenames"
+OPT_NO_MUSICBRAINZ = \
+    u"do not query MusicBrainz for metadata"
+OPT_NO_FREEDB = \
+    u"do not query FreeDB for metadata"
+OPT_INTERACTIVE_METADATA = \
+    u"edit metadata in interactive mode"
+OPT_INTERACTIVE_PLAY = \
+    u"play in interactive mode"
+OPT_OUTPUT_PLAY = \
+    u"the method to play audio (choose from: %s)"
+OPT_OUTPUT_TRACK2TRACK = \
+    u"output filename to use, overriding default and -d"
+OPT_OUTPUT_TRACKCAT = \
+    u"the output file"
+OPT_DEFAULT = \
+    u"when multiple choices are available, " + \
+    u"select the first one automatically"
+OPT_ALBUM_NUMBER = \
+    u"the album number of this disc, if it is one of a series of albums"
+OPT_ALBUM_TOTAL = \
+    u"the total albums of this disc\'s set, if it is one of a series of albums"
+OPT_REPLAY_GAIN = \
+    u"add ReplayGain metadata to newly created tracks"
+OPT_NO_REPLAY_GAIN = \
+    u"do not add ReplayGain metadata in newly created tracks"
+OPT_PLAYBACK_TRACK_GAIN = \
+    u"apply track ReplayGain during playback, if present"
+OPT_PLAYBACK_ALBUM_GAIN = \
+    u"apply album ReplayGain during playback, if present"
+OPT_SHUFFLE = \
+    u"shuffle tracks"
+OPT_PREFIX = \
+    u"add a prefix to the output image"
+OPT_NO_GTK = \
+    u"don't use PyGTK for GUI"
+OPT_NO_TKINTER = \
+    u"don't use Tkinter for GUI"
+OPT_AUDIO_TS = \
+    u"location of AUDIO_TS directory"
+OPT_DVDA_TITLE = \
+    u"DVD-Audio title number to extract tracks from"
+OPT_TRACK_START = \
+    u"the starting track number of the title being extracted"
+OPT_TRACK_TOTAL = \
+    u"the total number of tracks, if the extracted title is only a subset"
+OPT_SPEED = \
+    u"the speed to burn the CD at"
+OPT_CUESHEET_TRACK2CD = \
+    u"the cuesheet to use for writing tracks"
+OPT_JOINT = \
+    u"the maximum number of processes to run at a time"
+OPT_THUMBNAIL = \
+    u"convert embedded images to smaller thumbnails during conversion"
+OPT_CUESHEET_TRACKCAT = \
+    u"a cuesheet to embed in the output file"
+OPT_CUESHEET_TRACKSPLIT = \
+    u"the cuesheet to use for splitting track"
+OPT_NO_SUMMARY = \
+    u"suppress summary output"
+
+OPT_CAT_EXTRACTION = u"Extraction Options"
+OPT_CAT_CD_LOOKUP = u"CD Lookup Options"
+OPT_CAT_DVDA_LOOKUP = u"DVD-A Lookup Options"
+OPT_CAT_METADATA = u"Metadata Options"
+OPT_CAT_CONVERSION = u"Conversion Options"
+OPT_CAT_ENCODING = u"Encoding Options"
+
+#MetaData Fields
+METADATA_TRACK_NAME = u"Track Name"
+METADATA_TRACK_NUMBER = u"Track Number"
+METADATA_TRACK_TOTAL = u"Track Total"
+METADATA_ALBUM_NAME = u"Album Name"
+METADATA_ARTIST_NAME = u"Artist Name"
+METADATA_PERFORMER_NAME = u"Performer Name"
+METADATA_COMPOSER_NAME = u"Composer Name"
+METADATA_CONDUCTOR_NAME = u"Conductor Name"
+METADATA_MEDIA = u"Media"
+METADATA_ISRC = u"ISRC"
+METADATA_CATALOG = u"Catalog Number"
+METADATA_COPYRIGHT = u"Copyright"
+METADATA_PUBLISHER = u"Publisher"
+METADATA_YEAR = u"Release Year"
+METADATA_DATE = u"Recording Date"
+METADATA_ALBUM_NUMBER = u"Album Number"
+METADATA_ALBUM_TOTAL = u"Album Total"
+METADATA_COMMENT = u"Comment"
+
 
 #ReplayGain
 RG_ADDING_REPLAYGAIN = \
@@ -41,12 +193,215 @@ RG_REPLAYGAIN_APPLIED = \
 
 
 #Labels
+LAB_ENCODE = \
+    u"%(source)s -> %(destination)s"
+
 LAB_PICTURE = \
     u"Picture"
 
 LAB_T_OPTIONS = \
     u"Please use the -t option to specify %s"
 
+LAB_AVAILABLE_COMPRESSION_TYPES = \
+    u"Available compression types for %s:"
+
+LAB_CD2TRACK_PROGRESS = \
+    u"track %(track_number)2.2d -> %(filename)s"
+
+LAB_CD2TRACK_LOG = \
+    u"Rip log : "
+
+LAB_TOTAL_TRACKS = \
+    u"Total Tracks"
+
+LAB_TOTAL_LENGTH = \
+    u"Total Length"
+
+LAB_TRACK_LENGTH = \
+    u"%d:%2.2d"
+
+LAB_TRACK_LENGTH_FRAMES = \
+    u"%2d:%2.2d (%d frames)"
+
+LAB_FREEDB_ID = \
+    u"FreeDB disc ID"
+
+LAB_MUSICBRAINZ_ID = \
+    u"MusicBrainz disc ID"
+
+LAB_CDINFO_LENGTH = \
+    u"Length"
+
+LAB_CDINFO_FRAMES = \
+    u"Frames"
+
+LAB_CDINFO_OFFSET = \
+    u"Offset"
+
+LAB_PLAY_BUTTON = \
+    u"Play"
+
+LAB_PAUSE_BUTTON = \
+    u"Pause"
+
+LAB_NEXT_BUTTON = \
+    u"Next"
+
+LAB_PREVIOUS_BUTTON = \
+    u"Prev"
+
+LAB_PLAY_STATUS = \
+    u"%(count)d tracks, %(min)d:%(sec)2.2d minutes"
+
+LAB_PLAY_STATUS_1 = \
+    u"%(count)d track, %(min)d:%(sec)2.2d minutes"
+
+LAB_PLAY_TRACK = \
+    u"Track"
+
+LAB_CLOSE = \
+    u"Close"
+
+LAB_TRACK = \
+    u"track"
+
+LAB_X_OF_Y = \
+    u"%d / %d"
+
+LAB_CHOOSE_FILE = \
+    u"Choose an audio file"
+
+LAB_COVERVIEW_ABOUT = \
+    u"A viewer for displaying images embedded in audio files."
+
+LAB_AUDIOTOOLS_URL = \
+    u"http://audiotools.sourceforge.net"
+
+LAB_BYTE_SIZE = \
+    u"%d bytes"
+
+LAB_DIMENSIONS = \
+    u"%d \u00D7 %d"
+
+LAB_BITS_PER_PIXEL = \
+    u"%d bits"
+
+LAB_DVDA_TRACK = \
+    u"title %(title_number)d - track %(track_number)2.2d"
+
+LAB_DVDA_TITLE = \
+    u"Title %d"
+
+LAB_DVDA_TRACKS = \
+    u" (%d tracks)"
+
+LAB_DVDA_TITLE_INFO = \
+    u"%(minutes)2.2d:%(seconds)2.2d " + \
+    u"%(channels)dch %(rate)dHz %(bits)d-bit " + \
+    u"%(type)s"
+
+LAB_DVDAINFO_TITLE = \
+    u"Title"
+
+LAB_DVDAINFO_TRACK = \
+    u"Track"
+
+LAB_DVDAINFO_LENGTH = \
+    u"Length"
+
+LAB_DVDAINFO_FILENAME = \
+    u"Filename"
+
+LAB_DVDAINFO_STARTSECTOR = \
+    u"Start Sector"
+
+LAB_DVDAINFO_ENDSECTOR = \
+    u"End Sector"
+
+LAB_DVDAINFO_TICKS = \
+    u"PTS Ticks"
+
+LAB_CONVERTING_FILE = \
+    u"Converting audio file"
+
+LAB_TRACKCMP_CMP = \
+    u"%(file1)s <> %(file2)s"
+
+LAB_TRACKCMP_OK = \
+    u"OK"
+
+LAB_TRACKCMP_MISMATCH = \
+    u"differ at PCM frame %(frame_number)d"
+
+LAB_TRACKCMP_TYPE_MISMATCH = \
+    u"must be either files or directories"
+
+LAB_TRACKCMP_ERROR = \
+    u"error"
+
+LAB_TRACKCMP_MISSING = \
+    u"missing"
+
+LAB_TRACKCMP_RESULTS = \
+    u"Results:"
+
+LAB_TRACKCMP_HEADER_SUCCESS = \
+    u"success"
+
+LAB_TRACKCMP_HEADER_FAILURE = \
+    u"failure"
+
+LAB_TRACKCMP_HEADER_TOTAL = \
+    u"total"
+
+LAB_TRACKINFO_BITRATE = \
+    u"%(bitrate)4.4s kbps: %(filename)s"
+
+LAB_TRACKINFO_PERCENTAGE = \
+    u"%(percentage)3.3s%%: %(filename)s"
+
+LAB_TRACKINFO_ATTRIBS = \
+    u"%(minutes)2.2d:%(seconds)2.2d " + \
+    u"%(channels)dch %(rate)dHz %(bits)d-bit: %(filename)s"
+
+LAB_TRACKINFO_REPLAYGAIN = \
+    u"ReplayGain:"
+
+LAB_TRACKINFO_TRACK_GAIN = \
+    u"Track Gain : "
+
+LAB_TRACKINFO_TRACK_PEAK = \
+    u"Track Peak : "
+
+LAB_TRACKINFO_ALBUM_GAIN = \
+    u"Album Gain : "
+
+LAB_TRACKINFO_ALBUM_PEAK = \
+    u"Album Peak : "
+
+LAB_TRACKINFO_CUESHEET = \
+    u"Cuesheet:"
+
+LAB_TRACKINFO_CUESHEET_TRACK = \
+    u"track"
+
+LAB_TRACKINFO_CUESHEET_LENGTH = \
+    u"length"
+
+LAB_TRACKINFO_CUESHEET_ISRC = \
+    u"ISRC"
+
+LAB_TRACKINFO_CHANNELS = \
+    u"Assigned Channels:"
+
+LAB_TRACKINFO_CHANNEL = \
+    u"channel %(channel_number)d - %(channel_name)s"
+
+LAB_TRACKINFO_UNDEFINED = \
+    u"undefined"
+
+LAB_TRACKLENGTH = \
+    u"%(hours)d:%(minutes)2.2d:%(seconds)2.2d"
 
 #Compression settings
 COMP_FLAC_0 = \
@@ -97,6 +452,12 @@ COMP_WAVPACK_VERYHIGH = \
     u"slowest encode/decode, best compression"
 
 #Errors
+ERR_1_FILE_REQUIRED = \
+    u"You must specify exactly 1 supported audio file"
+
+ERR_FILES_REQUIRED = \
+    u"You must specify at least 1 supported audio file"
+
 ERR_UNSUPPORTED_CHANNEL_MASK = \
     u"Unable to write \"%(target_filename)s\" " + \
     u"with channel assignment \"%(assignment)s\""
@@ -112,11 +473,23 @@ ERR_UNSUPPORTED_CHANNEL_COUNT = \
 ERR_DUPLICATE_FILE = \
     u"File \"%s\" included more than once"
 
+ERR_OUTPUT_IS_INPUT = \
+    u"\"%s\" cannot be both input and output file"
+
 ERR_OPEN_IOERROR = \
     u"Unable to open \"%s\""
 
+ERR_ENCODING_ERROR = \
+    u"Unable to write \"%s\""
+
 ERR_UNSUPPORTED_AUDIO_TYPE = \
     u"Unsupported audio type \"%s\""
+
+ERR_UNSUPPORTED_FILE = \
+    u"Unsupported File '%s'"
+
+ERR_INVALID_FILE = \
+    u"Invalid File '%s'"
 
 ERR_AMBIGUOUS_AUDIO_TYPE = \
     u"Ambiguous suffix type \"%s\""
@@ -390,6 +763,121 @@ ERR_WAVPACK_INVALID_FMT = \
 
 ERR_WAVPACK_NO_FMT = \
     u"FMT chunk not found in WavPack"
+
+ERR_NO_COMPRESSION_MODES = \
+    u"Audio type %s has no compression modes"
+
+ERR_UNSUPPORTED_COMPRESSION_MODE = \
+    u"\"%(quality)s\" is not a supported compression mode " + \
+    u"for type \"%(type)s\""
+
+ERR_INVALID_CDDA = \
+    u". Is that an audio cd ?"
+
+ERR_NO_CDDA = \
+    u"No CD in drive"
+
+ERR_NO_EMPTY_CDDA = \
+    u"No audio tracks found on CD"
+
+ERR_NO_OUTPUT_FILE = \
+    u"You must specify an output file"
+
+ERR_DUPLICATE_OUTPUT_FILE = \
+    u"Output file \"%s\" occurs more than once"
+
+ERR_URWID_REQUIRED = \
+    u"urwid is required for interactive mode"
+
+ERR_GET_URWID1 = \
+    u"Please download and install urwid from http://excess.org/urwid/"
+
+ERR_GET_URWID2 = \
+    u"or your system's package manager."
+
+ERR_TERMIOS_ERROR = \
+    u"Unable to get tty settings"
+
+ERR_TERMIOS_SUGGESTION = \
+    u"If piping arguments via xargs(1), try using its -o option"
+
+ERR_NO_GUI = \
+    u"Neither PyGTK nor Tkinter is available"
+
+ERR_NO_AUDIO_TS = \
+    u"You must specify the DVD-Audio's AUDIO_TS directory with -A"
+
+ERR_INVALID_TITLE_NUMBER = \
+    u"title number must be greater than 0"
+
+ERR_INVALID_JOINT = \
+    u"You must run at least 1 process at a time"
+
+ERR_NO_CDRDAO = \
+    u"Unable to find \"cdrdao\" executable"
+
+ERR_GET_CDRDAO = \
+    u"Please install \"cdrdao\" to burn CDs"
+
+ERR_NO_CDRECORD = \
+    u"Unable to find \"cdrecord\" executable"
+
+ERR_GET_CDRECORD = \
+    u"Please install \"cdrecord\" to burn CDs"
+
+ERR_SAMPLE_RATE_MISMATCH = \
+    u"All audio files must have the same sample rate"
+
+ERR_CHANNEL_COUNT_MISMATCH = \
+    u"All audio files must have the same channel count"
+
+ERR_CHANNEL_MASK_MISMATCH = \
+    u"All audio files must have the same channel assignment"
+
+ERR_BPS_MISMATCH = \
+    u"All audio files must have the same bits per sample"
+
+ERR_TRACK2CD_INVALIDFILE = \
+    u"Not all files are valid.  Unable to write CD"
+
+ERR_TRACK2TRACK_O_AND_D = \
+    u"-o and -d options are not compatible"
+
+ERR_TRACK2TRACK_O_AND_D_SUGGESTION = \
+    u"Please specify either -o or -d but not both"
+
+ERR_TRACK2TRACK_O_AND_FORMAT = \
+    u"--format has no effect when used with -o"
+
+ERR_TRACK2TRACK_O_AND_MULTIPLE = \
+    u"You may specify only 1 input file for use with -o"
+
+ERR_NO_THUMBNAILS = \
+    u"Unable to generate thumbnails"
+
+ERR_THUMBNAIL_SUGGESTION1 = \
+    u"Please install the Python Imaging Library"
+
+ERR_THUMBNAIL_SUGGESTION2 = \
+    u"available at http://www.pythonware.com/products/pil/"
+
+ERR_THUMBNAIL_SUGGESTION3 = \
+    u"to enable image resizing"
+
+ERR_INVALID_THUMBNAIL_FORMAT = \
+    u"Unsupported thumbnail format \"%s\""
+
+ERR_AVAILABLE_THUMBNAIL_FORMATS = \
+    u"Available formats are: %s"
+
+ERR_TRACKCMP_TYPE_MISMATCH = \
+    u"Both files to be compared must be audio files"
+
+ERR_TRACKSPLIT_NO_CUESHEET = \
+    u"You must specify a cuesheet to split audio file"
+
+ERR_TRACKSPLIT_OVERLONG_CUESHEET = \
+    u"Cuesheet too long for track being split"
 
 #Cleaning messages
 
