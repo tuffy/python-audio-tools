57de223910b4e6f04aba8e39eb420a4be9dbfdb4
Brian Langenberger <bjl@usa.net>
2014-01-26 09:42:17 -0600

    Bump version to 2.21 beta 1

--- audiotools/__init__.py
+++ audiotools/__init__.py
@@ -131,7 +131,7 @@ MUSICBRAINZ_PORT = config.getint_default("MusicBrainz", "port", 80)
 ADD_REPLAYGAIN = config.getboolean_default("ReplayGain", "add_by_default",
                                            True)
 
-VERSION = "2.21alpha2"
+VERSION = "2.21beta1"
 
 DEFAULT_FILENAME_FORMAT = '%(track_number)2.2d - %(track_name)s.%(suffix)s'
 FILENAME_FORMAT = config.get_default("Filenames", "format",
