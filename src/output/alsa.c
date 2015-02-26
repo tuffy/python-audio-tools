5454646d845e6bcc3274982fb75d96d0986cff23
Brian Langenberger <bjl@usa.net>
2015-02-26 13:26:56 -0600

    Fix missing include in ALSA module

--- src/output/alsa.c
+++ src/output/alsa.c
@@ -1,5 +1,5 @@
 #include "alsa.h"
-#include "../pcmconv.h"
+#include "../framelist.h"
 
 /********************************************************
  Audio Tools, a module and set of tools for manipulating audio data
