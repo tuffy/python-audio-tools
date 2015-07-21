b07d60c972e74100dbeee286f5907dbcf8a78a5a
Brian Langenberger <bjl@usa.net>
2015-07-21 12:51:27 -0500

    Implement next set of M4A stream metadata atoms

--- src/common/m4a_atoms.c
+++ src/common/m4a_atoms.c
@@ -26,6 +26,12 @@ ATOM_DEF(mdhd)
 ATOM_DEF(hdlr)
 ATOM_DEF(smhd)
 ATOM_DEF(dref)
+ATOM_DEF(stsd)
+ATOM_DEF(alac)
+ATOM_DEF(stts)
+ATOM_DEF(stsc)
+ATOM_DEF(stsz)
+ATOM_DEF(stco)
 ATOM_DEF(free)
 
 static void
@@ -254,8 +260,7 @@ qt_smhd_new(void)
 }
 
 struct qt_atom*
-qt_dref_new(unsigned reference_atom_count,
-            ...)
+qt_dref_new(unsigned reference_atom_count, ...)
 {
     struct qt_atom *atom = malloc(sizeof(struct qt_atom));
     va_list ap;
@@ -276,6 +281,144 @@ qt_dref_new(unsigned reference_atom_count,
     atom->size = size_dref;
     atom->free = free_dref;
 
+    return atom;
+}
+
+struct qt_atom*
+qt_stsd_new(unsigned description_atom_count, ...)
+{
+    struct qt_atom *atom = malloc(sizeof(struct qt_atom));
+    va_list ap;
+
+    set_atom_name(atom, "stsd");
+    atom->type = QT_STSD;
+    atom->_.stsd = NULL;
+
+    va_start(ap, description_atom_count);
+    for (; description_atom_count; description_atom_count--) {
+        struct qt_atom *description_atom = va_arg(ap, struct qt_atom*);
+        atom->_.stsd = atom_list_append(atom->_.stsd, description_atom);
+    }
+    va_end(ap);
+
+    atom->display = display_stsd;
+    atom->build = build_stsd;
+    atom->size = size_stsd;
+    atom->free = free_stsd;
+
+    return atom;
+}
+
+struct qt_atom*
+qt_alac_new(unsigned max_samples_per_frame,
+            unsigned bits_per_sample,
+            unsigned history_multiplier,
+            unsigned initial_history,
+            unsigned maximum_K,
+            unsigned channels,
+            unsigned max_coded_frame_size,
+            unsigned bitrate,
+            unsigned sample_rate)
+{
+    struct qt_atom *atom = malloc(sizeof(struct qt_atom));
+    set_atom_name(atom, "alac");
+    atom->type = QT_ALAC;
+    atom->_.alac.max_samples_per_frame = max_samples_per_frame;
+    atom->_.alac.bits_per_sample = bits_per_sample;
+    atom->_.alac.history_multiplier = history_multiplier;
+    atom->_.alac.initial_history = initial_history;
+    atom->_.alac.maximum_K = maximum_K;
+    atom->_.alac.channels = channels;
+    atom->_.alac.max_coded_frame_size = max_coded_frame_size;
+    atom->_.alac.bitrate = bitrate;
+    atom->_.alac.sample_rate = sample_rate;
+    atom->display = display_alac;
+    atom->build = build_alac;
+    atom->size = size_alac;
+    atom->free = free_alac;
+    return atom;
+}
+
+struct qt_atom*
+qt_stts_new(unsigned times_count, ...)
+{
+    struct qt_atom *atom = malloc(sizeof(struct qt_atom));
+    unsigned i;
+    va_list ap;
+
+    set_atom_name(atom, "stts");
+    atom->type = QT_STTS;
+    atom->_.stts.times_count = times_count;
+    atom->_.stts.times = malloc(times_count * sizeof(struct stts_time));
+
+    va_start(ap, times_count);
+    for (i = 0; i < times_count; i++) {
+        atom->_.stts.times[i].occurences = va_arg(ap, unsigned);
+        atom->_.stts.times[i].pcm_frame_count = va_arg(ap, unsigned);
+    }
+    va_end(ap);
+
+    atom->display = display_stts;
+    atom->build = build_stts;
+    atom->size = size_stts;
+    atom->free = free_stts;
+    return atom;
+}
+
+struct qt_atom*
+qt_stsc_new(unsigned entries_count, ...)
+{
+    struct qt_atom *atom = malloc(sizeof(struct qt_atom));
+    unsigned i;
+    va_list ap;
+
+    set_atom_name(atom, "stsc");
+    atom->type = QT_STSC;
+    atom->_.stsc.entries_count = entries_count;
+    atom->_.stsc.entries = malloc(entries_count * sizeof(struct stsc_entry));
+
+    va_start(ap, entries_count);
+    for (i = 0; i < entries_count; i++) {
+        atom->_.stsc.entries[i].first_chunk = va_arg(ap, unsigned);
+        atom->_.stsc.entries[i].frames_per_chunk = va_arg(ap, unsigned);
+        atom->_.stsc.entries[i].description_index = 1;
+    }
+    va_end(ap);
+
+    atom->display = display_stsc;
+    atom->build = build_stsc;
+    atom->size = size_stsc;
+    atom->free = free_stsc;
+    return atom;
+}
+
+struct qt_atom*
+qt_stsz_new(unsigned frames_count)
+{
+    struct qt_atom *atom = malloc(sizeof(struct qt_atom));
+    set_atom_name(atom, "stsz");
+    atom->type = QT_STSZ;
+    atom->_.stsz.frames_count = frames_count;
+    atom->_.stsz.frame_size = calloc(frames_count, sizeof(unsigned));
+    atom->display = display_stsz;
+    atom->build = build_stsz;
+    atom->size = size_stsz;
+    atom->free = free_stsz;
+    return atom;
+}
+
+struct qt_atom*
+qt_stco_new(unsigned chunk_offsets)
+{
+    struct qt_atom *atom = malloc(sizeof(struct qt_atom));
+    set_atom_name(atom, "stco");
+    atom->type = QT_STCO;
+    atom->_.stco.offsets_count = chunk_offsets;
+    atom->_.stco.chunk_offset = calloc(chunk_offsets, sizeof(unsigned));
+    atom->display = display_stco;
+    atom->build = build_stco;
+    atom->size = size_stco;
+    atom->free = free_stco;
     return atom;
 }
 
@@ -928,6 +1071,342 @@ free_dref(struct qt_atom *self)
     free(self);
 }
 
+/*** stst ***/
+
+static void
+display_stsd(const struct qt_atom *self,
+             unsigned indent,
+             FILE *output)
+{
+    struct qt_atom_list *list;
+    display_indent(indent, output);
+    display_name(self->name, output);
+    fputs("\n", output);
+    for (list = self->_.stsd; list; list = list->next) {
+        list->atom->display(list->atom, indent + 1, output);
+    }
+}
+
+static void
+build_stsd(const struct qt_atom *self,
+           BitstreamWriter *stream)
+{
+    struct qt_atom_list *reference;
+
+    build_header(self, stream);
+    stream->write(stream, 8, 0);  /*version*/
+    stream->write(stream, 24, 0); /*flags*/
+    /*number of references*/
+    stream->write(stream, 32, atom_list_len(self->_.stsd));
+    for (reference = self->_.stsd; reference; reference = reference->next) {
+        reference->atom->build(reference->atom, stream);
+    }
+}
+
+static unsigned
+size_stsd(const struct qt_atom *self)
+{
+    unsigned size = 8 + 8;
+    struct qt_atom_list *reference;
+    for (reference = self->_.stsd;
+         reference;
+         reference = reference->next) {
+        size += reference->atom->size(reference->atom);
+    }
+    return size;
+}
+
+static void
+free_stsd(struct qt_atom *self)
+{
+    atom_list_free(self->_.stsd);
+    free(self);
+}
+
+/*** alac ***/
+
+static void
+display_alac(const struct qt_atom *self,
+             unsigned indent,
+             FILE *output)
+{
+    const char format[] = "     - %21s : %u\n";
+
+    display_indent(indent, output);
+    display_name(self->name, output);
+    fprintf(output, format + 4, "max samples per frame",
+            self->_.alac.max_samples_per_frame);
+
+    display_indent(indent, output);
+    fprintf(output, format, "bits-per-sample",
+            self->_.alac.bits_per_sample);
+
+    display_indent(indent, output);
+    fprintf(output, format, "history multiplier",
+            self->_.alac.history_multiplier);
+
+    display_indent(indent, output);
+    fprintf(output, format, "initial history",
+            self->_.alac.initial_history);
+
+    display_indent(indent, output);
+    fprintf(output, format, "maximum K",
+            self->_.alac.maximum_K);
+
+    display_indent(indent, output);
+    fprintf(output, format, "channels",
+            self->_.alac.channels);
+
+    display_indent(indent, output);
+    fprintf(output, format, "max coded frame size",
+            self->_.alac.max_coded_frame_size);
+
+    display_indent(indent, output);
+    fprintf(output, format, "bitrate",
+            self->_.alac.bitrate);
+
+    display_indent(indent, output);
+    fprintf(output, format, "sample rate",
+            self->_.alac.sample_rate);
+}
+
+static void
+build_alac(const struct qt_atom *self,
+           BitstreamWriter *stream)
+{
+    const uint8_t alac_id[4] = {0x61, 0x6C, 0x61, 0x63};
+
+    build_header(self, stream);
+    stream->write(stream, 48, 0);     /*reserved*/
+    stream->write(stream, 16, 1);     /*reference index*/
+    stream->write(stream, 16, 0);     /*version*/
+    stream->write(stream, 16, 0);     /*revision level*/
+    stream->write(stream, 32, 0);     /*vendor*/
+    stream->write(stream, 16, self->_.alac.channels);
+    stream->write(stream, 16, self->_.alac.bits_per_sample);
+    stream->write(stream, 16, 0);     /*compression ID*/
+    stream->write(stream, 16, 0);     /*audio packet size*/
+    stream->write(stream, 32, 44100); /*fake sample rate*/
+
+    stream->write(stream, 32, 36);    /*sub ALAC size*/
+    stream->write_bytes(stream, alac_id, 4);
+    stream->write(stream, 32, 0);     /*padding*/
+    stream->write(stream, 32, self->_.alac.max_samples_per_frame);
+    stream->write(stream, 8, 0);      /*padding*/
+    stream->write(stream, 8, self->_.alac.bits_per_sample);
+    stream->write(stream, 8, self->_.alac.history_multiplier);
+    stream->write(stream, 8, self->_.alac.initial_history);
+    stream->write(stream, 8, self->_.alac.maximum_K);
+    stream->write(stream, 8, self->_.alac.channels);
+    stream->write(stream, 16, 0x00FF); /*unknown*/
+    stream->write(stream, 32, self->_.alac.max_coded_frame_size);
+    stream->write(stream, 32, self->_.alac.bitrate);
+    stream->write(stream, 32, self->_.alac.sample_rate);
+}
+
+static unsigned
+size_alac(const struct qt_atom *self)
+{
+    return 72;
+}
+
+static void
+free_alac(struct qt_atom *self)
+{
+    free(self);
+}
+
+/*** stts ***/
+
+static void
+display_stts(const struct qt_atom *self,
+             unsigned indent,
+             FILE *output)
+{
+    unsigned i;
+    display_indent(indent, output);
+    display_name(self->name, output);
+    fprintf(output, " - %u entries\n", self->_.stts.times_count);
+    for (i = 0; i < self->_.stts.times_count; i++) {
+        display_indent(indent, output);
+        fprintf(output, "     - %u occurences, %u PCM frames\n",
+                self->_.stts.times[i].occurences,
+                self->_.stts.times[i].pcm_frame_count);
+    }
+}
+
+static void
+build_stts(const struct qt_atom *self,
+           BitstreamWriter *stream)
+{
+    unsigned i;
+
+    build_header(self, stream);
+    stream->write(stream, 8, 0);  /*version*/
+    stream->write(stream, 24, 0); /*flags*/
+    stream->write(stream, 32, self->_.stts.times_count);
+    for (i = 0; i < self->_.stts.times_count; i++) {
+        stream->write(stream, 32, self->_.stts.times[i].occurences);
+        stream->write(stream, 32, self->_.stts.times[i].pcm_frame_count);
+    }
+}
+
+static unsigned
+size_stts(const struct qt_atom *self)
+{
+    return 16 + self->_.stts.times_count * 8;
+}
+
+static void
+free_stts(struct qt_atom *self)
+{
+    free(self->_.stts.times);
+    free(self);
+}
+
+/*** stsc ***/
+
+static void
+display_stsc(const struct qt_atom *self,
+             unsigned indent,
+             FILE *output)
+{
+    unsigned i;
+    display_indent(indent, output);
+    display_name(self->name, output);
+    fprintf(output, " - %u entries\n", self->_.stts.times_count);
+    for (i = 0; i < self->_.stsc.entries_count; i++) {
+        display_indent(indent, output);
+        fprintf(output, "     - %u first chunk, %u frames per chunk\n",
+                self->_.stsc.entries[i].first_chunk,
+                self->_.stsc.entries[i].frames_per_chunk);
+    }
+
+}
+
+static void
+build_stsc(const struct qt_atom *self,
+           BitstreamWriter *stream)
+{
+    unsigned i;
+
+    build_header(self, stream);
+    stream->write(stream, 8, 0);  /*version*/
+    stream->write(stream, 24, 0); /*flags*/
+    stream->write(stream, 32, self->_.stsc.entries_count);
+    for (i = 0; i < self->_.stsc.entries_count; i++) {
+        stream->write(stream, 32, self->_.stsc.entries[i].first_chunk);
+        stream->write(stream, 32, self->_.stsc.entries[i].frames_per_chunk);
+        stream->write(stream, 32, self->_.stsc.entries[i].description_index);
+    }
+}
+
+static unsigned
+size_stsc(const struct qt_atom *self)
+{
+    return 16 + self->_.stsc.entries_count * 12;
+}
+
+static void
+free_stsc(struct qt_atom *self)
+{
+    free(self->_.stsc.entries);
+    free(self);
+}
+
+/*** stsz ***/
+
+static void
+display_stsz(const struct qt_atom *self,
+             unsigned indent,
+             FILE *output)
+{
+    unsigned i;
+
+    display_indent(indent, output);
+    display_name(self->name, output);
+    fprintf(output, " - %u sizes\n", self->_.stsz.frames_count);
+    for (i = 0; i < self->_.stsz.frames_count; i++) {
+        display_indent(indent, output);
+        fprintf(output, "     - %u) %u bytes\n", i, self->_.stsz.frame_size[i]);
+    }
+}
+
+static void
+build_stsz(const struct qt_atom *self,
+           BitstreamWriter *stream)
+{
+    unsigned i;
+
+    build_header(self, stream);
+    stream->write(stream, 8, 0);  /*version*/
+    stream->write(stream, 24, 0); /*flags*/
+    stream->write(stream, 32, 0); /*block byte size*/
+    stream->write(stream, 32, self->_.stsz.frames_count);
+    for (i = 0; i < self->_.stsz.frames_count; i++) {
+        stream->write(stream, 32, self->_.stsz.frame_size[i]);
+    }
+}
+
+static unsigned
+size_stsz(const struct qt_atom *self)
+{
+    return 20 + self->_.stsz.frames_count * 4;
+}
+
+static void
+free_stsz(struct qt_atom *self)
+{
+    free(self->_.stsz.frame_size);
+    free(self);
+}
+
+/*** stco ***/
+
+static void
+display_stco(const struct qt_atom *self,
+             unsigned indent,
+             FILE *output)
+{
+    unsigned i;
+
+    display_indent(indent, output);
+    display_name(self->name, output);
+    fprintf(output, " - %u offsets\n", self->_.stco.offsets_count);
+    for (i = 0; i < self->_.stco.offsets_count; i++) {
+        display_indent(indent, output);
+        fprintf(output, "     - %u) 0x%X\n", i, self->_.stco.chunk_offset[i]);
+    }
+}
+
+static void
+build_stco(const struct qt_atom *self,
+           BitstreamWriter *stream)
+{
+    unsigned i;
+
+    build_header(self, stream);
+    stream->write(stream, 8, 0);  /*version*/
+    stream->write(stream, 24, 0); /*flags*/
+    stream->write(stream, 32, self->_.stco.offsets_count);
+    for (i = 0; i < self->_.stco.offsets_count; i++) {
+        stream->write(stream, 32, self->_.stco.chunk_offset[i]);
+    }
+}
+
+static unsigned
+size_stco(const struct qt_atom *self)
+{
+    return 16 + self->_.stco.offsets_count * 4;
+}
+
+static void
+free_stco(struct qt_atom *self)
+{
+    free(self->_.stco.chunk_offset);
+    free(self);
+}
+
 /*** free ***/
 
 static void
@@ -967,12 +1446,9 @@ free_free(struct qt_atom *self)
 int
 main(int argc, char *argv[])
 {
-    uint8_t reference_atom_data[4] = {0, 0, 0, 1};
-
-    struct qt_atom *atom = qt_dref_new(1,
-                                       qt_leaf_new("url ",
-                                                   4,
-                                                   reference_atom_data));
+    //struct qt_atom *atom = qt_stts_new(2, 645, 4096, 1, 4080);
+    //struct qt_atom *atom = qt_stsc_new(2, 1, 5, 130, 1);
+    struct qt_atom *atom = qt_stco_new(5);
 
     BitstreamWriter *w = bw_open(stdout, BS_BIG_ENDIAN);
     atom->display(atom, 0, stderr);
--- src/common/m4a_atoms.h
+++ src/common/m4a_atoms.h
@@ -13,10 +13,18 @@ typedef enum {
   QT_HDLR,
   QT_SMHD,
   QT_DREF,
+  QT_STSD,
+  QT_ALAC,
+  QT_STTS,
+  QT_STSC,
+  QT_STSZ,
+  QT_STCO,
   QT_FREE
 } qt_atom_type_t;
 
 struct qt_atom_list;
+struct stts_time;
+struct stsc_entry;
 
 struct qt_atom {
     uint8_t name[4];
@@ -65,6 +73,40 @@ struct qt_atom {
 
         struct qt_atom_list *dref;
 
+        struct qt_atom_list *stsd;
+
+        struct {
+            unsigned max_samples_per_frame;
+            unsigned bits_per_sample;
+            unsigned history_multiplier;
+            unsigned initial_history;
+            unsigned maximum_K;
+            unsigned channels;
+            unsigned max_coded_frame_size;
+            unsigned bitrate;
+            unsigned sample_rate;
+        } alac;
+
+        struct {
+            unsigned times_count;
+            struct stts_time *times;
+        } stts;
+
+        struct {
+            unsigned entries_count;
+            struct stsc_entry *entries;
+        } stsc;
+
+        struct {
+            unsigned frames_count;
+            unsigned *frame_size;
+        } stsz;
+
+        struct {
+            unsigned offsets_count;
+            unsigned *chunk_offset;
+        } stco;
+
         unsigned free;
     } _;
 
@@ -88,6 +130,17 @@ struct qt_atom {
 struct qt_atom_list {
     struct qt_atom *atom;
     struct qt_atom_list *next;
+};
+
+struct stts_time {
+    unsigned occurences;
+    unsigned pcm_frame_count;
+};
+
+struct stsc_entry {
+    unsigned first_chunk;
+    unsigned frames_per_chunk;
+    unsigned description_index;
 };
 
 struct qt_atom*
@@ -134,8 +187,43 @@ struct qt_atom*
 qt_smhd_new(void);
 
 struct qt_atom*
-qt_dref_new(unsigned reference_atom_count,
-            ...);
+qt_dref_new(unsigned reference_atom_count, ...);
+
+struct qt_atom*
+qt_stsd_new(unsigned description_atom_count, ...);
+
+struct qt_atom*
+qt_alac_new(unsigned max_samples_per_frame,
+            unsigned bits_per_sample,
+            unsigned history_multiplier,
+            unsigned initial_history,
+            unsigned maximum_K,
+            unsigned channels,
+            unsigned max_coded_frame_size,
+            unsigned bitrate,
+            unsigned sample_rate);
+
+/*for each "times_count", there is both a frame_count and frame_duration
+  unsigned value which populates the atom*/
+struct qt_atom*
+qt_stts_new(unsigned times_count, ...);
+
+/*for each "entries_count" there is both a first_chunk and frames_per_chunk
+  unsigned value which populates the atom*/
+struct qt_atom*
+qt_stsc_new(unsigned entries_count, ...);
+
+/*generates a stsz atom whose frame sizes are all initialized at 0
+  one is expected to update them with actual values once
+  encoding is finished*/
+struct qt_atom*
+qt_stsz_new(unsigned frames_count);
+
+/*generates a stco atom whose chunk offsets are all initialized at 0
+  one is expected to update them with actual values once
+  encoding is finished*/
+struct qt_atom*
+qt_stco_new(unsigned chunk_offsets);
 
 struct qt_atom*
 qt_free_new(unsigned padding_bytes);
