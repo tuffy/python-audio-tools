6a1de84709c9ef5a2bf122c86f189e41c0a1a202
Brian Langenberger <bjl@usa.net>
2015-08-16 15:30:14 -0500

    Bump version to 3.1 beta 1
    Still a number of things to fix and clean up, but I think this
    release cycle is done with major changes for now.

--- audiotools/__init__.py
+++ audiotools/__init__.py
@@ -147,7 +147,7 @@ MUSICBRAINZ_PORT = config.getint_default("MusicBrainz", "port", 80)
 ADD_REPLAYGAIN = config.getboolean_default("ReplayGain", "add_by_default",
                                            True)
 
-VERSION = "3.1alpha1"
+VERSION = "3.1beta1"
 
 DEFAULT_FILENAME_FORMAT = '%(track_number)2.2d - %(track_name)s.%(suffix)s'
 FILENAME_FORMAT = config.get_default("Filenames", "format",
