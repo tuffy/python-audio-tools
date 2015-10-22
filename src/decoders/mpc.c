4624d4ccd6d9ee0214777d596d4a95f3dc31f7a1
James Buren <ryu0@ymail.com>
2015-10-22 00:58:06 -0500

    musepack: implement skeleton init, dealloc, new functions

--- src/decoders/mpc.c
+++ src/decoders/mpc.c
@@ -1,1 +1,27 @@
 #include "mpc.h"
+
+static PyObject*
+MPCDecoder_new(PyTypeObject *type,
+               PyObject *args, PyObject *kwds)
+{
+    decoders_MPCDecoder *self;
+
+    self = (decoders_MPCDecoder *)type->tp_alloc(type, 0);
+
+    return (PyObject *) self;
+}
+
+int
+MPCDecoder_init(decoders_MPCDecoder *self,
+                PyObject *args, PyObject *kwds)
+{
+    self->audiotools_pcm = NULL;
+
+    return 0;
+}
+
+void
+MPCDecoder_dealloc(decoders_MPCDecoder *self)
+{
+    Py_TYPE(self)->tp_free((PyObject*)self);
+}
