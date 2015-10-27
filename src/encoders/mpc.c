5d05e5e56a018b15aa3d403030cf071bc92f0079
James Buren <ryu0@ymail.com>
2015-10-27 00:27:45 -0500

    musepack: add standalone mpcenc to makefile

--- src/encoders/mpc.c
+++ src/encoders/mpc.c
@@ -1,8 +1,17 @@
 #include "../pcmreader.h"
 
+#ifndef STANDALONE
 PyObject*
 encoders_encode_mpc(PyObject *dummy, PyObject *args, PyObject *keywds)
 {
     Py_INCREF(Py_None);
     return Py_None;
 }
+#endif
+
+#ifdef STANDALONE
+int main(int argc, char *argv[])
+{
+    return 0;
+}
+#endif
