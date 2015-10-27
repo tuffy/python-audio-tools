9b46779d5169eaab4675771c4b0ccd328b1349d1
James Buren <ryu0@ymail.com>
2015-10-27 01:01:34 -0500

    musepack: implement checking pcmreader for supported sample rates, channels, and bits per sample

--- src/encoders/mpc.c
+++ src/encoders/mpc.c
@@ -2,17 +2,49 @@
 #include "../libmpcenc/libmpcenc.h"
 #include "../pcmreader.h"
 
-static void
+typedef enum {
+    ENCODE_OK,
+    ERR_UNSUPPORTED_SAMPLE_RATE,
+    ERR_UNSUPPORTED_CHANNELS,
+    ERR_UNSUPPORTED_BITS_PER_SAMPLE
+} result_t;
+
+static result_t
 encode_mpc_file(char *filename,
                 struct PCMReader *pcmreader)
 {
     PsyModel m;
     mpc_encoder_t e;
 
+    // Check for supported sample rates.
+    switch(pcmreader->sample_rate) {
+        case 32000:
+        case 37800:
+        case 44100:
+        case 48000: break;
+        default:    return ERR_UNSUPPORTED_SAMPLE_RATE;
+    }
+
+    // Check for supported channels.
+    switch(pcmreader->channels) {
+        case 1:
+        case 2:  break;
+        default: return ERR_UNSUPPORTED_CHANNELS;
+    }
+
+    // Check for supported bits per sample.
+    // TODO: add 8-32 bit support? the reference encoder supports it.
+    switch(pcmreader->bits_per_sample) {
+        case 16: break;
+        default: return ERR_UNSUPPORTED_BITS_PER_SAMPLE;
+    }
+
     // initialize tables
     m.SCF_Index_L = e.SCF_Index_L;
     m.SCF_Index_R = e.SCF_Index_R;
     Init_Psychoakustik(&m);
+
+    return ENCODE_OK;
 }
 
 #ifndef STANDALONE
